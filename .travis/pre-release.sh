8e278a170e01291fbe8dc0532668d54cce98c950
Stephen Fraser <stephenwf@users.noreply.github.com>
2019-06-10 17:20:06 +0100

    Local tool (for now) to build pre-release docker tags, without creating a latest tag to be manually pushed to Dockerhub. (#101)

---
+++ .travis/pre-release.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Next version from input
+NEXT_VERSION=$1;shift;
+
+# Create short commit hash for tagging.
+COMMIT_HASH="$(git rev-parse --short HEAD)"
+
+echo "Building v$NEXT_VERSION-$COMMIT_HASH";
+
+# Build.
+docker build -t madoc-platform-prerelease .
+
+# Tag.
+docker tag madoc-platform-prerelease:latest digirati/madoc-platform:v"$NEXT_VERSION"-"$COMMIT_HASH"
+echo -e
+echo -e "Successfully created and tagged: digirati/madoc-platform:v$NEXT_VERSION-$COMMIT_HASH";
+echo -e
+echo -e "  To push run:"
+echo -e "  $ docker push digirati/madoc-platform:v$NEXT_VERSION-$COMMIT_HASH"
+echo -e
