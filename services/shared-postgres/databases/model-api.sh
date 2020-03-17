b2eb3d02072ee1c777d360a681b6750c927038c6
Stephen Fraser <stephen.fraser@digirati.com>
2020-03-17 09:36:31 +0000

    Removed shell scripts for database

--- services/shared-postgres/databases/model-api.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-"EOSQL"
-	CREATE USER model_api WITH ENCRYPTED PASSWORD 'model_api_passowrd';
-  CREATE DATABASE model_api;
-  GRANT ALL PRIVILEGES ON DATABASE model_api TO model_api;
-EOSQL
