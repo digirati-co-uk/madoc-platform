ddad2ac921bf8f6ec78bb5c92b6f474c4b35b73f
Stephen Fraser <stephen.fraser@digirati.com>
2020-03-29 03:34:11 +0100

    Removed comment

--- services/madoc-ts/src/omeka/Media.ts
+++ services/madoc-ts/src/omeka/Media.ts
@@ -1,24 +1,3 @@
-/*
-create table media
-(
-	id int not null
-		primary key,
-	item_id int not null,
-	ingester varchar(255) not null,
-	renderer varchar(255) not null,
-	data longtext null comment '(DC2Type:json_array)',
-	source longtext null,
-	media_type varchar(255) null,
-	storage_id varchar(190) null,
-	extension varchar(255) null,
-	sha256 char(64) null,
-	has_original tinyint(1) not null,
-	has_thumbnails tinyint(1) not null,
-	position int null,
-	lang varchar(190) null,
-	size bigint null,
-
- */
 export type Media = {
   id: number;
   item_id: number;
