4d9ae3d1d3ee79ef6c9945a001c7ecded87c9301
Stephen Fraser <stephen.fraser@digirati.com>
2020-04-06 17:39:43 +0100

    Added login and logout pages that set JWT

--- services/madoc-ts/src/router.ts
+++ services/madoc-ts/src/router.ts
@@ -3,13 +3,22 @@ import { ping } from './routes/ping';
 import { omekaHelloWorld } from './routes/omeka-hello-world';
 import { madocNotFound } from './routes/madoc-not-found';
 import { keys } from './routes/keys';
+import { loginPage } from './routes/user/login';
+import { getSiteScopes, saveSiteScopes } from './routes/admin/site-scopes';
+import { logout } from './routes/user/logout';
 
 export const router = new TypedRouter({
   // Normal route
   'get-ping': [TypedRouter.GET, '/api/madoc', ping],
   'get-scopes': [TypedRouter.GET, '/api/madoc/site/:siteId/permissions', getSiteScopes],
   'update-scopes': [TypedRouter.POST, '/api/madoc/site/:siteId/permissions', saveSiteScopes],
+
   'omeka-test': [TypedRouter.GET, '/s/:slug/madoc/hello-world', omekaHelloWorld],
   'get-keys': [TypedRouter.GET, '/s/:slug/madoc/test-key', keys],
+  'get-login': [TypedRouter.GET, '/s/:slug/madoc/login', loginPage],
+  'post-login': [TypedRouter.POST, '/s/:slug/madoc/login', loginPage],
+  'get-logout': [TypedRouter.GET, '/s/:slug/madoc/logout', logout],
+
+  // Make sure this is last.
   'omeka-404': [TypedRouter.GET, '/s/:slug/madoc*', madocNotFound],
 });
