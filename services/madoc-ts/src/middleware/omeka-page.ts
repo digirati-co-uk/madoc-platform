cc7c5b718e68be90bbad6392984c03cd1a4c77de
Stephen Fraser <stephen.fraser@digirati.com>
2020-04-06 17:35:57 +0100

    Updated Omeka page integration to support messages and JWT

--- services/madoc-ts/src/middleware/omeka-page.ts
+++ services/madoc-ts/src/middleware/omeka-page.ts
@@ -4,21 +4,45 @@ import fetch from 'node-fetch';
 const omekaUrl = process.env.OMEKA__URL as string;
 
 export const omekaPage: RouteMiddleware<{ slug: string }> = async (context, next) => {
+  context.omekaMessages = [];
+
   await next();
 
-
-  if (typeof context.omekaPage !== 'undefined' && context.params.slug) {
+  // Only want to enable this from the context of the madoc site.
+  if (typeof context.omekaPage !== 'undefined' && context.params && context.params.slug) {
+    // We fetch from Omeka, passing both the cookie and JWT (in-case Omeka is not yet aware of the user.)
     const response = await fetch(`${omekaUrl}/s/${context.params.slug}/_template`, {
-      headers: context.req.headers as any,
-    }).then(r => r.text());
+      headers: {
+        cookie: context.req.headers.cookie ? context.req.headers.cookie.toString() : '',
+        Authorization: context.state.jwt ? `Bearer ${context.state.jwt.token}` : '',
+      },
+    });
 
-    const [header, footer] = response.split('<!--{{ content }}-->');
+    // If Omeka is logged in, but the application is not, then we should return the login page, where this will bee
+    // picked up and the JWT for the logged in user will be created.
+    const userId = Number(response.headers.get('X-Authenticated-User-Id'));
+    if (userId && !context.state.jwt && !context.query.redirect) {
+      // Redirects back to the page we are on now.
+      context.response.redirect(
+        context.routes.url('get-login', { slug: context.params.slug }, { query: { redirect: context.request.url } })
+      );
+      return;
+    }
 
+    // Grab the HTML of the Omeka template.
+    const html = await response.text();
+
+    // Split it by the comment indicating the content.
+    const [header, footer] = html.split('<!--{{ content }}-->');
+
+    // Return the response wrapped in Omeka.
     context.response.body = `
       ${header}
+      ${(context.omekaMessages || []).map(
+        ({ type, message }) => `<ul class="messages messages--body"><li class="${type}">${message}</li></ul>`
+      )}
       ${context.omekaPage}
       ${footer}
-      source: ${`${omekaUrl}/s/${context.params.slug}/_template`}
     `;
   }
 };
--- services/madoc-ts/src/routes/omeka-hello-world.ts
+++ services/madoc-ts/src/routes/omeka-hello-world.ts
@@ -2,30 +2,6 @@ import { RouteMiddleware } from '../types';
 
 export const omekaHelloWorld: RouteMiddleware<{ slug: string }> = async context => {
   context.omekaPage = `
-    <div class="c-form c-form--login">
-      <h1 class="c-form__heading">Login from typescript</h1>
-      <form method="POST" name="loginform" id="loginform">
-        <div class="field required">
-          <div class="field-meta">
-            <label for="email">Email</label>
-          </div>
-          <div class="inputs">
-            <input type="email" name="email" required="required" id="email" value="">    
-          </div>
-        </div>
-        <div class="field required">
-          <div class="field-meta">
-            <label for="password">Password</label>
-          </div>
-          <div class="inputs">
-            <input type="password" name="password" required="required" id="password" value="">    
-          </div>
-        </div>
-        <input type="submit" name="submit" value="Log in"><input type="hidden" name="redir" value="">
-      </form>
-      <p class="c-form__forgot-password">
-        <a href="/forgot-password">Forgot password?</a>
-      </p>
-    </div>
+    <h1>Hello from Madoc</h1>
   `;
 };
