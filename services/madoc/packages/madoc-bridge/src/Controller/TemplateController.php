760457b66614fb84f60b344ecbf717c100f6ec80
Stephen Fraser <stephen.fraser@digirati.com>
2020-04-06 17:24:52 +0100

    Add custom header to template with user ID

--- services/madoc/packages/madoc-bridge/src/Controller/TemplateController.php
+++ services/madoc/packages/madoc-bridge/src/Controller/TemplateController.php
@@ -3,13 +3,35 @@
 namespace MadocBridge\Controller;
 
 use Digirati\OmekaShared\Framework\AbstractPsr7ActionController;
+use Zend\Http\Headers;
+use Zend\Http\Request;
+use Zend\Http\Response;
+use Zend\Http\Response as ZendResponse;
+use Zend\Mvc\MvcEvent;
+use Zend\Psr7Bridge\Psr7Response;
 use Zend\View\Model\ViewModel;
 
 class TemplateController extends AbstractPsr7ActionController
 {
     public function viewAction()
     {
-        // Any additional data?
+
+        $user = $this->getCurrentUser();
         return new ViewModel();
+    }
+
+    public function onDispatch(MvcEvent $e)
+    {
+        $result = parent::onDispatch($e);
+
+        $response = $e->getResponse();
+        if ($this->getCurrentUser()) {
+            $headers = $response->getHeaders();
+            /** @var Request $req */
+            $req = $e->getRequest();
+            $reqHeaders = $req->getHeaders()->toArray() ?? [];
+            $headers->addHeaderLine('X-Authenticated-User-Id', $this->getCurrentUser()->getId());
+        }
+        return $result;
     }
 }
