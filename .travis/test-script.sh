5baf0ec567bc9a5fa1ff6ce0ab256027d788cbb9
Stephen Fraser <stephen.fraser@digirati.com>
2020-04-06 19:13:15 +0100

    Removed Madoc TS from smoke test (not in CI compose yet)

--- .travis/test-script.sh
+++ .travis/test-script.sh
@@ -6,6 +6,3 @@ set -ex
 
 # Test example site
 [[ "$(curl -sS "http://localhost:8888/s/default/page/homepage" --output /dev/null --write-out '%{http_code}' 2>&1)" == "200" ]]
-
-# Test Madoc TS.
-[[ "$(curl -sS "http://localhost:8888/s/default/madoc/login" --output /dev/null --write-out '%{http_code}' 2>&1)" == "200" ]]
