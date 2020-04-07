328745fa603c323b980d65d59ba3c07130901c4d
Stephen Fraser <stephen.fraser@digirati.com>
2020-04-07 22:09:18 +0100

    Fixed get single task

--- services/tasks-api/src/routes/get-single-task.ts
+++ services/tasks-api/src/routes/get-single-task.ts
@@ -2,7 +2,7 @@ import { RouteMiddleware } from '../types';
 import { getTask } from '../database/get-task';
 
 export const getSingleTask: RouteMiddleware<{ id: string }> = async context => {
-  context.response.body = getTask(context.connection, {
+  context.response.body = await getTask(context.connection, {
     context: context.state.jwt.context,
     user: context.state.jwt.user,
     id: context.params.id,
