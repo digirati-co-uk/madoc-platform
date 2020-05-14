47cd3ad42f1e47203f80d6363a2d772c3033e208
Stephen Fraser <stephen.fraser@digirati.com>
2020-05-14 18:29:10 +0100

    Removing unused types

--- services/madoc-ts/src/omeka-custom/OAuthAccessTokens.ts
+++
@@ -1,7 +0,0 @@
-export type OAuthAccessTokens = {
-  access_token: string;
-  client_id: string;
-  user_id: string;
-  expires: Date;
-  scope: string;
-};
--- services/madoc-ts/src/omeka-custom/OAuthAuthorizationCodes.ts
+++
@@ -1,8 +0,0 @@
-export type OAuthAuthorizationCodes = {
-  authorization_code: string;
-  client_id: string;
-  user_id: string;
-  redirect_uri: string;
-  expires: Date;
-  scope: string;
-};
