63f637035e7c81372b00833584e09c46c747dc44
Stephen Fraser <stephen.fraser@digirati.com>
2020-05-20 11:39:27 +0100

    Remove unused files

--- services/madoc-ts/src/router.ts
+++ services/madoc-ts/src/router.ts
@@ -1,8 +1,6 @@
 import { TypedRouter } from './utility/typed-router';
 import { ping } from './routes/ping';
-import { omekaHelloWorld } from './routes/omeka-hello-world';
 import { madocNotFound } from './routes/madoc-not-found';
-import { keys } from './routes/keys';
 import { loginPage } from './routes/user/login';
 import { getSiteScopes, saveSiteScopes } from './routes/admin/site-scopes';
 import { logout } from './routes/user/logout';
@@ -91,8 +89,6 @@ export const router = new TypedRouter({
   'get-canvas-metadata': [TypedRouter.GET, '/api/madoc/iiif/canvases/:id/metadata', getCanvasMetadata],
   'put-canvas-metadata': [TypedRouter.PUT, '/api/madoc/iiif/canvases/:id/metadata', updateMetadata, 'MetadataUpdate'],
 
-  'omeka-test': [TypedRouter.GET, '/s/:slug/madoc/hello-world', omekaHelloWorld],
-  'get-keys': [TypedRouter.GET, '/s/:slug/madoc/test-key', keys],
   'get-login': [TypedRouter.GET, '/s/:slug/madoc/login', loginPage],
   'post-login': [TypedRouter.POST, '/s/:slug/madoc/login', loginPage],
   'get-logout': [TypedRouter.GET, '/s/:slug/madoc/logout', logout],
