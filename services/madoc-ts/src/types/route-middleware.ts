514e8cde6c14eff14f25bbcf593998337e5d50df
Stephen Fraser <stephen.fraser@digirati.com>
2020-05-26 13:53:32 +0100

    Fixed formatting

--- services/madoc-ts/src/types/route-middleware.ts
+++ services/madoc-ts/src/types/route-middleware.ts
@@ -3,8 +3,10 @@ import { ApplicationState } from './application-state';
 import { ApplicationContext } from './application-context';
 import { RouterParamContext } from '@koa/router';
 
-export type RouteMiddleware<Params = any, Body = any> = Koa.Middleware<ApplicationState,
+export type RouteMiddleware<Params = any, Body = any> = Koa.Middleware<
+  ApplicationState,
   ApplicationContext &
-  Omit<RouterParamContext<ApplicationState, ApplicationContext>, 'params'> & { params: Params } & {
-  requestBody: Body;
-}>;
+    Omit<RouterParamContext<ApplicationState, ApplicationContext>, 'params'> & { params: Params } & {
+      requestBody: Body;
+    }
+>;
