635ef720feb55d1170ddcbc97944159d77f3e79d
Stephen Fraser <stephen.fraser@digirati.com>
2020-05-26 13:52:44 +0100

    Added parent id to count resources

--- services/madoc-ts/src/database/queries/resource-queries.ts
+++ services/madoc-ts/src/database/queries/resource-queries.ts
@@ -1,6 +1,18 @@
 import { sql } from 'slonik';
 
-export function countResources(resource_type: string, site_id: number) {
+export function countResources(resource_type: string, site_id: number, parent_id?: number) {
+  if (parent_id) {
+    return sql<{ total: number }>`
+    select count(*) as total
+      from iiif_derived_resource cidr
+        left join iiif_derived_resource_items cidri 
+            on cidr.resource_id = cidri.item_id
+      where cidr.resource_type = ${resource_type} 
+      and cidr.site_id = ${site_id}
+      and cidri.resource_id = ${parent_id}
+  `;
+  }
+
   return sql<{ total: number }>`
     select count(*) as total
       from iiif_derived_resource
