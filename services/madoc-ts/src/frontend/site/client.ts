907e05fa9f7328ded66f41772251415d47a5ee7d
Stephen Fraser <stephen.fraser@digirati.com>
2020-06-11 15:00:32 +0100

    Frontend site

---
+++ services/madoc-ts/src/frontend/site/client.ts
@@ -0,0 +1,23 @@
+import { renderClient } from '../shared/utility/render-client';
+
+Promise.all([
+  // The component.
+  import('./index'),
+  // The routes themselves.
+  import('./routes'),
+  // And the components
+  import('./components'),
+]).then(([mod, routes, components]) => {
+  // For plugins, I'm expecting this is where they would be detected and loaded.
+  // - Get array of plugins
+  // - Run hooks for routes and components
+  // - Pass modified parts to render client.
+  // - The config for a site will tell us:
+  //    - Plugins to load
+  //    - Config for those plugins
+  //    - Theme configuration
+  // - This will be picked up in a script tag for the SSR in some way.
+  // - A plugin could in theory be a remote URL too, and have some fancy server-side caching
+  //   of them.
+  renderClient(mod.default, routes.createRoutes(components), false);
+});
---
+++ services/madoc-ts/src/frontend/site/index.tsx
@@ -0,0 +1,32 @@
+import React, { useMemo } from 'react';
+import { ApiClient } from '../../gateway/api';
+import { useTranslation } from 'react-i18next';
+import { renderUniversalRoutes } from '../shared/utility/server-utils';
+import { ApiContext, useIsApiRestarting } from '../shared/hooks/use-api';
+import { ErrorMessage } from '../shared/atoms/ErrorMessage';
+import { UniversalRoute } from '../types';
+import { VaultProvider } from '@hyperion-framework/react-vault';
+
+export type SiteAppProps = {
+  jwt?: string;
+  api: ApiClient;
+  routes: UniversalRoute[];
+};
+
+const SiteApp: React.FC<SiteAppProps> = ({ api, routes }) => {
+  const { i18n } = useTranslation();
+  const restarting = useIsApiRestarting(api);
+
+  const viewingDirection = useMemo(() => i18n.dir(i18n.language), [i18n.language]);
+
+  return (
+    <VaultProvider>
+      <div lang={i18n.language} dir={viewingDirection}>
+        {restarting ? <ErrorMessage>Lost connection to server, retrying... </ErrorMessage> : null}
+        <ApiContext.Provider value={api}>{renderUniversalRoutes(routes)}</ApiContext.Provider>
+      </div>
+    </VaultProvider>
+  );
+};
+
+export default SiteApp;
