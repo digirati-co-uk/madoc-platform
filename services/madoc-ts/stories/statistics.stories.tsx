b867cf81e19959e683944863855323db016d8e12
Stephen Fraser <stephen.fraser@digirati.com>
2020-06-17 22:09:43 +0100

    Added storybook for statistics

---
+++ services/madoc-ts/stories/statistics.stories.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import {
+  Statistic,
+  StatisticContainer,
+  StatisticLabel,
+  StatisticNumber,
+} from '../src/frontend/shared/atoms/Statistics';
+
+export default { title: 'Statistics' };
+
+export const simpleList = () => {
+  return (
+    <StatisticContainer>
+      <Statistic>
+        <StatisticNumber>32</StatisticNumber>
+        <StatisticLabel>Not started</StatisticLabel>
+      </Statistic>
+      <Statistic>
+        <StatisticNumber>4</StatisticNumber>
+        <StatisticLabel>In progress</StatisticLabel>
+      </Statistic>
+      <Statistic>
+        <StatisticNumber>6</StatisticNumber>
+        <StatisticLabel>In review</StatisticLabel>
+      </Statistic>
+      <Statistic>
+        <StatisticNumber>7</StatisticNumber>
+        <StatisticLabel>Completed</StatisticLabel>
+      </Statistic>
+    </StatisticContainer>
+  );
+};
