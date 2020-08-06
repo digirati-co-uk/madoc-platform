061518485178edd5dc873a658a278e5ed0833a3d
Stephen Fraser <stephen.fraser@digirati.com>
2020-08-06 22:22:49 +0100

    Bug fixes with new env

--- services/madoc-ts/migrate.js
+++ services/madoc-ts/migrate.js
@@ -2,13 +2,13 @@ const { setupSlonikMigrator } = require('@slonik/migrator');
 const { createPool } = require('slonik');
 
 const slonik = createPool(
-    `postgres://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`
+  `postgres://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`
 );
 
 const migrator = setupSlonikMigrator({
-    migrationsPath: __dirname + '/migrations',
-    slonik,
-    mainModule: module,
+  migrationsPath: __dirname + '/migrations',
+  slonik,
+  mainModule: module,
 });
 
 module.exports = { slonik, migrator };
