ed957346b60d36e2529dead5c2f7590f5795d953
Stephen Fraser <stephen.fraser@digirati.com>
2020-08-10 23:24:39 +0100

    Updated project listing view

---
+++ services/madoc-ts/src/frontend/shared/atoms/ProjectListing.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import styled from 'styled-components';
+import { LocaleString } from '../components/LocaleString';
+import { Button } from './Button';
+import { useTranslation } from 'react-i18next';
+import { HrefLink } from '../utility/href-link';
+import { ProjectSnippet } from '../../../types/schemas/project-snippet';
+
+const ProjectListingContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const ProjectListingTitle = styled.div`
+  font-size: 1.2em;
+  margin-bottom: 0.5em;
+  a {
+    text-decoration: none;
+  }
+`;
+
+const ProjectListingDescription = styled.div`
+  font-size: 0.85em;
+  margin-bottom: 1em;
+`;
+
+const ProjectListingItem = styled.div`
+  background: #eee;
+  padding: 1em;
+  & ~ & {
+    margin-top: 1em;
+  }
+`;
+
+export const ProjectListing: React.FC<{
+  projects: ProjectSnippet[];
+  onContribute: (id: string | number) => void;
+}> = ({ projects, onContribute }) => {
+  const { t } = useTranslation();
+
+  return (
+    <ProjectListingContainer>
+      {projects.map(project => (
+        <ProjectListingItem key={project.id}>
+          <ProjectListingTitle>
+            <HrefLink href={`/projects/${project.id}`}>
+              <LocaleString>{project.label}</LocaleString>
+            </HrefLink>
+          </ProjectListingTitle>
+          <ProjectListingDescription>
+            <LocaleString>{project.summary}</LocaleString>
+          </ProjectListingDescription>
+          <Button onClick={() => onContribute(project.id)}>{t('Contribute')}</Button>
+        </ProjectListingItem>
+      ))}
+    </ProjectListingContainer>
+  );
+};
---
+++ services/madoc-ts/stories/projects.stories.tsx
@@ -0,0 +1,37 @@
+import { ProjectList } from '../src/types/schemas/project-list';
+import * as React from 'react';
+import { ProjectListing } from '../src/frontend/shared/atoms/ProjectListing';
+import { MemoryRouter } from 'react-router-dom';
+
+export default { title: 'Projects' };
+
+const projects: ProjectList['projects'] = [
+  {
+    id: 1234,
+    label: { en: ['My first project'] },
+    summary: { en: ['Example summary'] },
+    capture_model_id: '1231231231231',
+    collection_id: 123,
+    task_id: '1231231231',
+    slug: '123',
+  },
+  {
+    id: 1236,
+    label: { en: ['My second project'] },
+    summary: { en: ['Example summary'] },
+    capture_model_id: '1231231231231',
+    collection_id: 123,
+    task_id: '1231231231',
+    slug: '123',
+  },
+];
+
+export const ExampleProjectSnippets: React.FC = () => {
+  return (
+    <MemoryRouter>
+      <div style={{ padding: '2em' }}>
+        <ProjectListing projects={projects} onContribute={id => console.log('chose', id)} />
+      </div>
+    </MemoryRouter>
+  );
+};
