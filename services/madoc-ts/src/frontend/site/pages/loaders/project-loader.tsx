d6aafac8ccfcf3e3a661c1655933f3f91f237478
Stephen Fraser <stephen.fraser@digirati.com>
2020-08-12 18:12:08 +0100

    Added tasks, collections and manifests to project loader

--- services/madoc-ts/src/frontend/site/pages/loaders/project-loader.tsx
+++ services/madoc-ts/src/frontend/site/pages/loaders/project-loader.tsx
@@ -4,35 +4,54 @@ import { UniversalComponent } from '../../../types';
 import { createUniversalComponent } from '../../../shared/utility/create-universal-component';
 import { useStaticData } from '../../../shared/hooks/use-data';
 import { BreadcrumbContext } from '../../../shared/components/Breadcrumbs';
+import { ProjectFull } from '../../../../types/schemas/project-full';
+import { ProjectTask } from '../../../../types/tasks/project-task';
+import { CollectionFull } from '../../../../types/schemas/collection-full';
 
 type ProjectLoaderType = {
   params: { slug: string };
   query: {};
   variables: { slug: string };
-  data: any;
-  context: { project: any };
+  data: {
+    task: ProjectTask;
+    project: ProjectFull;
+    collections: CollectionFull;
+    manifests: CollectionFull;
+  };
+  context: { project: ProjectFull };
 };
 
 export const ProjectLoader: UniversalComponent<ProjectLoaderType> = createUniversalComponent<ProjectLoaderType>(
   ({ route }) => {
     const { data } = useStaticData(ProjectLoader);
 
-    const ctx = useMemo(() => (data ? { id: data.id, name: data.label } : undefined), [data]);
+    const ctx = useMemo(() => (data ? { id: data.project.id, name: data.project.label } : undefined), [data]);
 
     if (!data) {
       return <div>Loading...</div>;
     }
 
-    return (
-      <BreadcrumbContext project={ctx}>{renderUniversalRoutes(route.routes, { project: data })}</BreadcrumbContext>
-    );
+    return <BreadcrumbContext project={ctx}>{renderUniversalRoutes(route.routes, data)}</BreadcrumbContext>;
   },
   {
     getKey: params => {
       return ['site-project', { slug: params.slug }];
     },
-    getData: (key, variables, api) => {
-      return api.getSiteProject(variables.slug);
+    getData: async (key, variables, api) => {
+      const project = await api.getSiteProject(variables.slug);
+
+      const [task, collections, manifests] = await Promise.all([
+        await api.getTaskById<ProjectTask>(project.task_id),
+        await api.getSiteCollection(project.collection_id, { type: 'collection' }),
+        await api.getSiteCollection(project.collection_id, { type: 'manifest' }),
+      ]);
+
+      return {
+        project: await api.getSiteProject(variables.slug),
+        collections,
+        manifests,
+        task,
+      };
     },
   }
 );
