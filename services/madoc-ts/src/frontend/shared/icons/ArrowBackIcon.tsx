5611d0fc0047b233994fe5c0b06dbe8f1a9c5294
Stephen Fraser <stephen.fraser@digirati.com>
2020-08-17 08:57:35 +0100

    Added new set of icons for review workflow

---
+++ services/madoc-ts/src/frontend/shared/icons/ArrowBackIcon.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export function ArrowBackIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg height="1em" width="1em" {...props}>
+      <path d="M0 0h24v24H0z" fill="none" />
+      <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z" />
+    </svg>
+  );
+}
