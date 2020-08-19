dd6535340faf239c992e83a151c6ea6b56869d1f
Stephen Fraser <stephen.fraser@digirati.com>
2020-08-19 15:40:23 +0100

    Fixed refreshing on multi-review

--- services/madoc-ts/src/frontend/site/pages/tasks/crowdsourcing-multi-review.tsx
+++ services/madoc-ts/src/frontend/site/pages/tasks/crowdsourcing-multi-review.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useCallback, useMemo } from 'react';
 import { useQuery } from 'react-query';
 import { CrowdsourcingReview } from '../../../../gateway/tasks/crowdsourcing-review';
 import { useApi } from '../../../shared/hooks/use-api';
@@ -14,6 +14,10 @@ import { useLocationQuery } from '../../../shared/hooks/use-location-query';
 import { PreviewCrowdsourcingTask } from './preview-crowdsourcing-task.lazy';
 import { ApiClient } from '../../../../gateway/api';
 import { RevisionRequest } from '@capture-models/types';
+import { MergeCrowdsourcingTask } from './merge-crowdsourcing-task.lazy';
+import { WarningMessage } from '../../../shared/atoms/WarningMessage';
+import { Button } from '@storybook/react/demo';
+import { LinkButton } from '../../../shared/atoms/Button';
 
 // Step 1 - review each and wait for completion
 // Step 2 - request any changes on individual items or remove them
@@ -32,7 +36,7 @@ export const CrowdsourcingMultiReview: React.FC<{ task: CrowdsourcingReview; ref
   const { slug } = useParams();
   const history = useHistory();
   const { preview } = useLocationQuery();
-  const { data } = useQuery(['multi-review-tasks', { id: reviewTask.id }], async () => {
+  const { data, refetch: refetchTask } = useQuery(['multi-review-tasks', { id: reviewTask.id }], async () => {
     return await api.getTasks<CrowdsourcingTask>(0, {
       all: true,
       // status: [-1, 0, 1, 2, 3, 4],
@@ -41,8 +45,20 @@ export const CrowdsourcingMultiReview: React.FC<{ task: CrowdsourcingReview; ref
       detail: true,
     });
   });
-
+  const lockedTasks = useMemo(
+    () =>
+      reviewTask.state.currentMerge
+        ? [...reviewTask.state.currentMerge.toMerge, reviewTask.state.currentMerge.revisionId]
+        : [],
+    [reviewTask.state.currentMerge]
+  );
   const previewTask = data ? data.tasks.find(t => t.id === preview) : undefined;
+
+  const refreshAll = useCallback(() => {
+    return Promise.all(
+      [refetchTask({ force: true }), refetch ? refetch() : null].filter(r => r !== null) as Promise<void>[]
+    );
+  }, [refetch, refetchTask]);
 
   if (!data) {
     return <div>Loading...</div>;
@@ -50,6 +66,26 @@ export const CrowdsourcingMultiReview: React.FC<{ task: CrowdsourcingReview; ref
 
   if (reviewTask.state.baseRevisionId) {
     return <div>Base revision chosen</div>;
+  }
+
+  if (reviewTask.state.currentMerge && reviewTask.state.currentMerge.mergeId === preview) {
+    return (
+      <Suspense fallback={<div>Loading...</div>}>
+        <MergeCrowdsourcingTask
+          merge={reviewTask.state.currentMerge}
+          goBack={async opt => {
+            if (opt?.refresh) {
+              await refreshAll();
+            }
+            const rev = opt?.revisionId;
+            history.push(
+              createLink({ projectId: slug, taskId: reviewTask.id, query: rev ? { preview: rev } : undefined })
+            );
+          }}
+          reviewTaskId={reviewTask.id as string}
+        />
+      </Suspense>
+    );
   }
 
   const waiting = data.tasks.filter(t => t.status <= 1 || t.status === 4);
@@ -62,13 +98,18 @@ export const CrowdsourcingMultiReview: React.FC<{ task: CrowdsourcingReview; ref
       <Suspense fallback={<div>Loading...</div>}>
         <PreviewCrowdsourcingTask
           allTaskIds={allTaskIds}
+          lockedTasks={lockedTasks}
           allRevisionIds={allRevisionIds}
+          allTasks={ready}
           task={previewTask as any}
           goBack={async opt => {
             if (opt?.refresh && refetch) {
-              await refetch();
+              await refreshAll();
             }
-            history.push(createLink({ projectId: slug, taskId: reviewTask.id }));
+            const rev = opt?.revisionId;
+            history.push(
+              createLink({ projectId: slug, taskId: reviewTask.id, query: rev ? { preview: rev } : undefined })
+            );
           }}
           reviewTaskId={reviewTask.id as string}
         />
@@ -78,6 +119,20 @@ export const CrowdsourcingMultiReview: React.FC<{ task: CrowdsourcingReview; ref
 
   return (
     <div>
+      {reviewTask.state.currentMerge ? (
+        <WarningMessage>
+          There is a merge in progress{' '}
+          <Link
+            to={createLink({
+              projectId: slug,
+              taskId: reviewTask.id,
+              query: { preview: reviewTask.state.currentMerge.mergeId },
+            })}
+          >
+            Go to merge
+          </Link>
+        </WarningMessage>
+      ) : null}
       <Heading3>This review covers the following contributions</Heading3>
       <GridContainer>
         <HalfGird $margin>
