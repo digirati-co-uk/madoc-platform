9ff6846494f3586535432458d38361298132f1e5
Stephen Fraser <stephen.fraser@digirati.com>
2020-08-19 23:43:54 +0100

    Adding links when creating manifests canvases and collections

--- services/madoc-ts/src/routes/iiif/canvases/create-canvas.ts
+++ services/madoc-ts/src/routes/iiif/canvases/create-canvas.ts
@@ -2,6 +2,8 @@ import { userWithScope } from '../../../utility/user-with-scope';
 import { sql } from 'slonik';
 import { RouteMiddleware } from '../../../types/route-middleware';
 import { CreateCanvas } from '../../../types/schemas/create-canvas';
+import { addLinks } from '../../../database/queries/linking-queries';
+import { extractLinks } from '../../../utility/extract-links';
 
 // @todo return full canvas.
 export const createCanvas: RouteMiddleware<{}, CreateCanvas> = async context => {
@@ -15,6 +17,16 @@ export const createCanvas: RouteMiddleware<{}, CreateCanvas> = async context => 
   const { canonical_id } = await context.connection.one<{ derived_id: number; canonical_id: number }>(
     sql`select * from create_canvas(${canvasJson}, ${localSource}, ${thumbnail}, ${siteId}, ${userUrn})`
   );
+
+  try {
+    // Links.
+    const links = addLinks(extractLinks(context.requestBody.canvas as any, 'iiif'), canonical_id, siteId);
+    if (links) {
+      await context.connection.query(links);
+    }
+  } catch (err) {
+    console.log(err);
+  }
 
   context.response.body = { id: canonical_id };
   context.response.status = 201;
--- services/madoc-ts/src/routes/iiif/manifests/create-manifest.ts
+++ services/madoc-ts/src/routes/iiif/manifests/create-manifest.ts
@@ -2,6 +2,8 @@ import { sql } from 'slonik';
 import { userWithScope } from '../../../utility/user-with-scope';
 import { RouteMiddleware } from '../../../types/route-middleware';
 import { CreateManifest } from '../../../types/schemas/create-manifest';
+import { addLinks } from '../../../database/queries/linking-queries';
+import { extractLinks } from '../../../utility/extract-links';
 
 export const createManifest: RouteMiddleware<{}, CreateManifest> = async context => {
   const { userUrn, siteId } = userWithScope(context, ['site.admin']);
@@ -13,6 +15,17 @@ export const createManifest: RouteMiddleware<{}, CreateManifest> = async context
   const { canonical_id } = await context.connection.one<{ derived_id: number; canonical_id: number }>(
     sql`select * from create_manifest(${manifestJson}, ${localSource}, ${siteId}, ${userUrn}, ${taskId})`
   );
+
+  try {
+    const extracted = extractLinks(context.requestBody.manifest as any, 'iiif');
+    // Links.
+    const links = addLinks(extracted, canonical_id, siteId);
+    if (links) {
+      await context.connection.query(links);
+    }
+  } catch (err) {
+    console.log(err);
+  }
 
   context.response.body = { id: canonical_id };
   context.response.status = 201;
