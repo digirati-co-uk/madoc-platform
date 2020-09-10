80efa27ab613e607570353796c770ec54cbcd07b
Stephen Fraser <stephen.fraser@digirati.com>
2020-09-10 16:56:54 +0100

    Added manifest crowdsourcing review

--- services/madoc-ts/src/frontend/site/pages/tasks/crowdsourcing-review.tsx
+++ services/madoc-ts/src/frontend/site/pages/tasks/crowdsourcing-review.tsx
@@ -2,25 +2,23 @@ import React from 'react';
 import { CrowdsourcingReview } from '../../../../gateway/tasks/crowdsourcing-review';
 import TimeAgo from 'react-timeago';
 import { TaskContext } from '../loaders/task-loader';
+import { CrowdsourcingManifestReview } from './crowdsourcing-manifest-review';
 import { CrowdsourcingMultiReview } from './crowdsourcing-multi-review';
 import { Heading1, Subheading1 } from '../../../shared/atoms/Heading1';
 
 export const ViewCrowdsourcingReview: React.FC<TaskContext<CrowdsourcingReview>> = ({ task, refetch: refetchTask }) => {
-  return (
-    <div>
-      <Heading1>{task.name}</Heading1>
-      {task.created_at ? (
-        <Subheading1>
-          <TimeAgo date={task.created_at} />
-        </Subheading1>
-      ) : null}
+  const reviewType = task.parameters[1] || 'canvas';
 
-      <CrowdsourcingMultiReview
-        task={task}
-        refetch={async () => {
-          await refetchTask();
-        }}
-      />
-    </div>
+  if (reviewType === 'manifest') {
+    return <CrowdsourcingManifestReview task={task as any} />;
+  }
+
+  return (
+    <CrowdsourcingMultiReview
+      task={task}
+      refetch={async () => {
+        await refetchTask();
+      }}
+    />
   );
 };
