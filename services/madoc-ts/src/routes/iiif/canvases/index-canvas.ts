4f541f28dab6a35925e96f8f763597201afbfbdc
Kelsie <kelsie.braidwood@digirati.com>
2020-10-17 17:53:50 +0100

    Ingest working without build errors

--- services/madoc-ts/src/routes/iiif/canvases/index-canvas.ts
+++ services/madoc-ts/src/routes/iiif/canvases/index-canvas.ts
@@ -1,63 +1,50 @@
 import { sql } from 'slonik';
-import {
-  getManifestSnippets,
-  getSingleManifest,
-  mapManifestSnippets,
-} from '../../../database/queries/get-manifest-snippets';
 import { api } from '../../../gateway/api.server';
 import { RouteMiddleware } from '../../../types/route-middleware';
 import { optionalUserWithScope } from '../../../utility/user-with-scope';
+import { getParentResources } from '../../../database/queries/resource-queries';
+import { mapMetadata, MetadataField } from '../../../utility/iiif-metadata';
 
 export const indexCanvas: RouteMiddleware<{ id: string }> = async context => {
   const { siteId, siteUrn } = optionalUserWithScope(context, []);
   const userApi = api.asUser({ siteId });
   const canvasId = Number(context.params.id);
 
-  //  NEED TO WORK OUT WHAT TO DO HERE INSTEAD. WE WANT TO BE ABLE TO GET A SINGLE CANVAS OUT.
-  const rows = await context.connection.any(
-    getManifestSnippets(
-      getSingleManifest({
-        canvasId,
-        siteId: Number(siteId),
-        perPage: 1,
-        page: 1,
-        excludeCanvases: [],
-      }),
-      {
-        siteId: Number(siteId),
-        fields: ['label'],
-        allManifestFields: true,
-      }
-    )
+  const canvases = await context.connection.any<MetadataField>(sql`
+    select
+            ifd.resource_id,
+            ifd.created_at,
+            im.key,
+            im.value,
+            im.language,
+            im.source from iiif_derived_resource ifd
+        left outer join iiif_metadata im on ifd.resource_id = im.resource_id
+    where ifd.site_id ~ ${siteId}
+    and ifd.resource_type = 'canvas'
+    and im.key = 'label'
+    and im.site_id = ${siteId}
+    group by ifd.resource_id, ifd.created_at, im.key, im.value, im.language, im.source
+    `);
+
+  const canvas = mapMetadata(canvases)[0];
+
+  const manifestsWithin = await context.connection.any(
+    getParentResources(`http://madoc.dev/urn:madoc:canvas:${canvasId}`, siteId)
   );
-
-  const table = mapManifestSnippets(rows);
-
-  if (!table.canvases[`${canvasId}`]) {
-    return; // no metadata to index.
-  }
-
-  const collectionsWithin = await context.connection.any<{ resource_id: number }>(
-    sql`select cols.resource_id from iiif_derived_resource_items cols
-        left join iiif_derived_resource ir on ir.resource_id = cols.resource_id
-        where item_id = ${canvasId} and cols.site_id = ${Number(siteId)} and ir.flat = false`
-  );
-
-  const manifest = table.canvases[`${canvasId}`];
 
   const searchPayload = {
     id: `urn:madoc:canvas:${canvasId}`,
     type: 'Canvas',
     resource: {
-      ...manifest,
       id: `http://madoc.dev/urn:madoc:canvas:${canvasId}`,
       type: 'Canvas',
+      ...canvas,
     },
-    thumbnail: manifest.thumbnail,
+    thumbnail: canvas.default_thumbnail,
     contexts: [
       { id: siteUrn, type: 'Site' },
-      ...collectionsWithin.map(({ resource_id }) => {
-        return { id: `urn:madoc:collection:${resource_id}`, type: 'Collection' };
+      ...manifestsWithin.map(({ resource_id }) => {
+        return { id: `urn:madoc:manifests:${resource_id}`, type: 'Manifests' };
       }),
       {
         id: `urn:madoc:canvas:${canvasId}`,
@@ -78,5 +65,5 @@ export const indexCanvas: RouteMiddleware<{ id: string }> = async context => {
     console.log(e);
   }
 
-  context.response.body = table.canvases[`${canvasId}`];
+  context.response.body = canvas;
 };
