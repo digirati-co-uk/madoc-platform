ec37b8dae4c8e0b310d0046de2bac7d2d734e760
Stephen Fraser <stephen.fraser@digirati.com>
2020-10-26 22:05:22 +0000

    MAD-444 Fixed canvas search ingest

--- services/madoc-ts/src/routes/iiif/canvases/index-canvas.ts
+++ services/madoc-ts/src/routes/iiif/canvases/index-canvas.ts
@@ -1,51 +1,38 @@
-import { sql } from 'slonik';
 import { api } from '../../../gateway/api.server';
 import { RouteMiddleware } from '../../../types/route-middleware';
+import { SearchIngestRequest } from '../../../types/search';
 import { optionalUserWithScope } from '../../../utility/user-with-scope';
 import { getParentResources } from '../../../database/queries/resource-queries';
-import { mapMetadata, MetadataField } from '../../../utility/iiif-metadata';
 
 export const indexCanvas: RouteMiddleware<{ id: string }> = async context => {
   const { siteId, siteUrn } = optionalUserWithScope(context, []);
   const userApi = api.asUser({ siteId });
   const canvasId = Number(context.params.id);
 
-  const canvases = await context.connection.any<MetadataField>(sql`
-    select
-            ifd.resource_id,
-            ifd.created_at,
-            im.key,
-            im.value,
-            im.language,
-            im.source from iiif_derived_resource ifd
-        left outer join iiif_metadata im on ifd.resource_id = im.resource_id
-    where ifd.site_id ~ ${siteId}
-    and ifd.resource_type = 'canvas'
-    and im.key = 'label'
-    and im.site_id = ${siteId}
-    group by ifd.resource_id, ifd.created_at, im.key, im.value, im.language, im.source
-    `);
+  const { canvas } = await userApi.getCanvasById(canvasId);
 
-  const canvas = mapMetadata(canvases)[0];
-
-  const manifestsWithin = await context.connection.any(
-    getParentResources(`http://madoc.dev/urn:madoc:canvas:${canvasId}`, siteId)
-  );
-
-  const searchPayload = {
+  const manifestsWithin = await context.connection.any(getParentResources(canvasId as any, siteId));
+  const searchPayload: SearchIngestRequest = {
     id: `urn:madoc:canvas:${canvasId}`,
     type: 'Canvas',
     resource: {
-      id: `http://madoc.dev/urn:madoc:canvas:${canvasId}`,
       type: 'Canvas',
-      ...canvas,
+      id: `http://madoc.dev/urn:madoc:canvas:${canvasId}`,
+      label: canvas.label as any,
+      thumbnail: canvas.thumbnail as any,
+      summary: canvas.summary,
+      metadata: canvas.metadata,
+      rights: (canvas as any).rights,
+      provider: (canvas as any).provider,
+      requiredStatement: canvas.requiredStatement,
+      navDate: (canvas as any).navDate,
     },
-    thumbnail: canvas.default_thumbnail,
+    thumbnail: canvas.thumbnail as string,
     contexts: [
       { id: siteUrn, type: 'Site' },
       // Should this be contexts or manifests here? Do canvases have site contexts too?
       ...manifestsWithin.map(({ resource_id }) => {
-        return { id: `urn:madoc:manifests:${resource_id}`, type: 'Manifests' };
+        return { id: `urn:madoc:manifest:${resource_id}`, type: 'Manifest' };
       }),
       {
         id: `urn:madoc:canvas:${canvasId}`,
@@ -55,15 +42,11 @@ export const indexCanvas: RouteMiddleware<{ id: string }> = async context => {
   };
 
   try {
+    await api.searchGetIIIF(`urn:madoc:canvas:${canvasId}`);
+
+    await userApi.searchReIngest(searchPayload);
+  } catch (err) {
     await userApi.searchIngest(searchPayload);
-    const alreadyIndexed = (await api.getIndexedCanvasById(`urn:madoc:canvas:${canvasId}`)).results.length > 0;
-    if (alreadyIndexed) {
-      await userApi.searchReIngest(searchPayload);
-    } else {
-      await userApi.searchIngest(searchPayload);
-    }
-  } catch (e) {
-    console.log(e);
   }
 
   context.response.body = canvas;
