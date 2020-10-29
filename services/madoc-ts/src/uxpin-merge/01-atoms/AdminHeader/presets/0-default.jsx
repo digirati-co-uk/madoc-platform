012a246d6b2cc8b4cd9aea684c21f6afb99f6400
Stephen Fraser <stephen.fraser@digirati.com>
2020-10-29 17:20:08 +0000

    Preset change again

--- services/madoc-ts/src/uxpin-merge/01-atoms/AdminHeader/presets/0-default.jsx
+++ services/madoc-ts/src/uxpin-merge/01-atoms/AdminHeader/presets/0-default.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import AdminHeader from '../AdminHeader';
 
 export default (
-  <AdminHeader title="My awesome project" breadcrumbs="site dashboard" menu="menu 1" activeItem="Overview" />
+  <AdminHeader title="My awesome project" breadcrumbs="site dashboard" menu="menu 1" activeItem="Overview"> </AdminHeader>
 );
