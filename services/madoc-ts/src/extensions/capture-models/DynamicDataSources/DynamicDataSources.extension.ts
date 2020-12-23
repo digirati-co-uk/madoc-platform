c9f04233eef28c89aa63b2fdd46dc4ce338e0af8
Stephen Fraser <stephen.fraser@digirati.com>
2020-12-23 18:47:39 +0000

    Added dynamic data source extension + plaintext

---
+++ services/madoc-ts/src/extensions/capture-models/DynamicDataSources/DynamicDataSources.extension.ts
@@ -0,0 +1,74 @@
+import { traverseDocument } from '@capture-models/helpers';
+import { BaseField, CaptureModel } from '@capture-models/types';
+import { ApiClient } from '../../../gateway/api';
+import { CaptureModelExtension } from '../extension';
+import { DynamicData } from './types';
+
+export class DynamicDataSourcesExtension implements CaptureModelExtension {
+  api: ApiClient;
+  dataLoaders: DynamicData[];
+  constructor(api: ApiClient, dataLoaders: DynamicData[]) {
+    this.api = api;
+    this.dataLoaders = dataLoaders;
+  }
+
+  async onCloneCaptureModel(captureModel: CaptureModel): Promise<CaptureModel> {
+    if (this.dataLoaders.length === 0) {
+      return captureModel;
+    }
+
+    const dataLoaderTypes = this.dataLoaders.map(loader => loader.definition.id);
+
+    const state = {
+      fields: [] as Array<{ field: BaseField; key: string; source: string }>,
+    };
+
+    // 1. Does the capture model support data sources.
+    traverseDocument(captureModel.document, {
+      visitField(field, key) {
+        const dataSources = field.dataSources
+          ? field.dataSources.filter(source => dataLoaderTypes.indexOf(source) !== -1)
+          : undefined;
+
+        // 2. Do any of the fields have data sources?
+        if (dataSources) {
+          for (const source of dataSources) {
+            state.fields.push({
+              field,
+              key,
+              source,
+            });
+          }
+        }
+      },
+    });
+
+    if (state.fields.length === 0 || !captureModel.id) {
+      return captureModel;
+    }
+
+    // 3. Resolve data needed about the resource
+    const { canvas } = this.api.parseModelTarget(captureModel.target);
+
+    if (!canvas) {
+      // No valid target.
+      return captureModel;
+    }
+
+    // 4. Call each of the extensions to get the data from the source
+    for (const field of state.fields) {
+      const loader = this.dataLoaders.find(candidateLoader => candidateLoader.definition.id === field.source);
+      // Just double check.
+      if (loader) {
+        try {
+          // 5. Modify and return the model.
+          await loader.loader(field.field, field.key, canvas, this.api);
+        } catch (err) {
+          console.log('Loader error', err);
+        }
+      }
+    }
+
+    return await this.api.updateCaptureModel(captureModel.id, captureModel);
+  }
+}
