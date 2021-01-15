6b2c7f1886457de70f4943be3742be731dc3ce32
Stephen Fraser <stephen.fraser@digirati.com>
2021-01-15 10:39:50 +0000

    No tasks

--- services/madoc-ts/src/routes/user/details.ts
+++ services/madoc-ts/src/routes/user/details.ts
@@ -7,11 +7,19 @@ export const userDetails: RouteMiddleware<{ slug: string }> = async context => {
 
   const userApi = api.asUser({ siteId, userId: id });
 
-  const statistics = await userApi.getAllTaskStats({ user_id: userUrn, type: 'crowdsourcing-task' });
+  try {
+    const statistics = await userApi.getAllTaskStats({ user_id: userUrn, type: 'crowdsourcing-task' });
 
-  context.response.body = {
-    ...(await context.omeka.getUser(id)),
-    currentSiteId: siteId,
-    statistics,
-  };
+    context.response.body = {
+      ...(await context.omeka.getUser(id)),
+      currentSiteId: siteId,
+      statistics,
+    };
+  } catch (err) {
+    context.response.body = {
+      ...(await context.omeka.getUser(id)),
+      currentSiteId: siteId,
+      statistics: { total: 0, statuses: {} },
+    };
+  }
 };
