451f96b8cca0545fc9d48679ae08b6f8d37ff3c2
Stephen Fraser <stephen.fraser@digirati.com>
2021-01-18 10:34:40 +0000

    Split out table handle icon

--- services/madoc-ts/src/frontend/shared/atoms/ReorderTable.tsx
+++ services/madoc-ts/src/frontend/shared/atoms/ReorderTable.tsx
@@ -1,25 +1,8 @@
 import React from 'react';
 import { DragDropContext, Droppable, Draggable, DropResult } from 'react-beautiful-dnd';
 import styled from 'styled-components';
+import { TableHandleIcon } from '../icons/TableHandleIcon';
 import { TableActions, TableContainer, TableRow, TableRowLabel } from './Table';
-
-const TableHandleIcon: React.FC<{ className: string } & any> = ({ className, ...props }) => (
-  <div className={className} {...props}>
-    <svg width="17px" height="11px" viewBox="0 0 17 11" version="1.1">
-      <title>Handle</title>
-      <g id="Symbols" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-        <g id="Compact-item" transform="translate(-10.000000, -10.000000)">
-          <rect id="Rectangle" stroke="#B1B1B1" x="0.5" y="0.5" width="1197" height="29" />
-          <g id="Handle" transform="translate(10.000000, 10.000000)" stroke="#979797" strokeLinecap="square">
-            <line x1="16.5" y1="0.5" x2="0.5" y2="0.5" id="Line-5" />
-            <line x1="16.5" y1="5.5" x2="0.5" y2="5.5" id="Line-5" />
-            <line x1="16.5" y1="10.5" x2="0.5" y2="10.5" id="Line-5" />
-          </g>
-        </g>
-      </g>
-    </svg>
-  </div>
-);
 
 const TableHandle = styled(TableHandleIcon)`
   margin: 4px;
