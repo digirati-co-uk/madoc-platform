e20328c3cb792cb39ad8e6c8da85a5cd11bf1efb
Stephen Fraser <stephen.fraser@digirati.com>
2021-01-19 01:24:28 +0000

    Added not found page

--- services/madoc-ts/src/frontend/shared/components/NotFoundPage.tsx
+++ services/madoc-ts/src/frontend/shared/components/NotFoundPage.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
-export const NotFoundPage: React.FC = () => {
+export const NotFoundPage: React.FC<{ error?: any }> = () => {
   return <h1>Not found</h1>;
+};
+
+export const ErrorPage: React.FC<{ error?: any }> = () => {
+  return (
+    <>
+      <h1>An error occurred</h1>
+    </>
+  );
 };
