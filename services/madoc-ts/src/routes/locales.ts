690d926138f6f993f65a3b92420870db64726feb
Stephen Fraser <stephen.fraser@digirati.com>
2021-02-12 16:27:37 +0000

    Missing translations

--- services/madoc-ts/src/routes/locales.ts
+++ services/madoc-ts/src/routes/locales.ts
@@ -1,5 +1,6 @@
 // /api/madoc/locales/{{lng}}/{{ns}}
 
+import deepmerge from 'deepmerge';
 import * as path from 'path';
 import { existsSync, readFileSync, writeFileSync } from 'fs';
 import send from 'koa-send';
@@ -28,23 +29,28 @@ export const saveMissingLocale: RouteMiddleware<{ lng: string; ns: string }> = a
   }
 
   if (context.state.jwt && context.state.jwt.scope.indexOf('site.admin') !== -1) {
-    try {
-      const bundle = path.resolve(
-        __dirname,
-        '..',
-        '..',
-        'translations',
-        context.params.lng,
-        `${context.params.ns}.json`
-      );
-      if (existsSync(bundle)) {
-        const bundleJson = JSON.parse(readFileSync(bundle).toString('utf-8'));
-        const newBundle = { ...bundleJson, ...context.requestBody };
-        writeFileSync(bundle, JSON.stringify(newBundle, Object.keys(newBundle).sort(), 2));
+    // During development we want to add missing translations to the local file to commit.
+    if (process.env.NODE_ENV !== 'production') {
+      try {
+        const bundle = path.resolve(
+          __dirname,
+          '..',
+          '..',
+          'translations',
+          context.params.lng,
+          `${context.params.ns}.json`
+        );
+        if (existsSync(bundle)) {
+          const bundleJson = JSON.parse(readFileSync(bundle).toString('utf-8'));
+          const newBundle = deepmerge(bundleJson, context.requestBody);
+          writeFileSync(bundle, JSON.stringify(newBundle, Object.keys(newBundle as any).sort(), 2));
+        }
+      } catch (e) {
+        console.log(e);
+        // fail silently.
       }
-    } catch (e) {
-      console.log(e);
-      // fail silently.
+    } else {
+      // @todo store dynamic translations in a table.
     }
   }
 
