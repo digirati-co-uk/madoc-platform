f1e1ae89ea0d070f1588e6e66df0cb02ed9aedc9
Stephen Fraser <stephen.fraser@digirati.com>
2021-02-17 15:53:32 +0000

    MAD-600 Fixed wording in configuration

--- services/madoc-ts/src/frontend/shared/configuration/site-config.ts
+++ services/madoc-ts/src/frontend/shared/configuration/site-config.ts
@@ -2,14 +2,14 @@ export const siteConfigurationModel = {
   allowCollectionNavigation: {
     label: 'Collection navigation',
     description:
-      'Allow users to navigate through the collections. If this option is enabled then users will have to be assigned canvases or manifests to see them, or optionally pick up a random resource.',
+      'Allow users to navigate through the collections. If this option is disabled then users will have to be assigned canvases or manifests to see them, or optionally pick up a random resource.',
     type: 'checkbox-field',
     inlineLabel: 'Allow collection navigation',
   },
   allowManifestNavigation: {
     label: 'Manifest navigation',
     description:
-      'Allow users to navigate through a manifest of images. If this option is enabled then users will have to be assigned canvases or the whole manifest to navigate them.',
+      'Allow users to navigate through a manifest of images. If this option is disabled then users will have to be assigned canvases or the whole manifest to navigate them.',
     type: 'checkbox-field',
     inlineLabel: 'Allow manifest navigation',
   },
