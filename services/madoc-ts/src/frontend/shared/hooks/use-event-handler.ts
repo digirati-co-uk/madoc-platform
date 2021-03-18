264c4eb5c1aaa2d28ae1512a7df34ec0b98956fa
Stephen Fraser <stephen.fraser@digirati.com>
2021-03-18 12:16:03 +0000

    Expanded resize layout hook

--- services/madoc-ts/src/frontend/shared/hooks/use-event-handler.ts
+++ services/madoc-ts/src/frontend/shared/hooks/use-event-handler.ts
@@ -3,12 +3,13 @@ import { useLayoutEffect } from 'react';
 export function useEventHandler(ref: any, name: string, callback: (e: any) => void, deps: any[] = []) {
   useLayoutEffect(() => {
     const current = ref.current;
-    current.addEventListener(name, callback);
+    if (current) {
+      current.addEventListener(name, callback);
 
-    return () => {
-      current.removeEventListener(name, callback);
-    };
-
+      return () => {
+        current.removeEventListener(name, callback);
+      };
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [name, ...deps]);
 }
--- services/madoc-ts/src/frontend/shared/hooks/use-resize-layout.ts
+++ services/madoc-ts/src/frontend/shared/hooks/use-resize-layout.ts
@@ -2,6 +2,10 @@ import { useRef, useState } from 'react';
 import { useEventHandler } from './use-event-handler';
 import { useLocalStorage } from './use-local-storage';
 
+function distance(x1: number, y1: number, x2: number, y2: number) {
+  return Math.sqrt(Math.pow(Math.abs(x2 - x1), 2) + Math.pow(Math.abs(y2 - y1), 2));
+}
+
 function getMaxWidthPct(container: number, defaultMaxWidthPct: number, maxWidthPct?: number, maxWidthPixel?: number) {
   if (maxWidthPct) {
     return maxWidthPct;
@@ -27,20 +31,23 @@ export function useResizeLayout(
     onDragEnd?: () => void;
   } = {}
 ) {
-  const container = useRef<HTMLDivElement>();
-  const resizableDiv = useRef<HTMLDivElement>();
-  const otherDiv = useRef<HTMLDivElement>();
-  const resizer = useRef<HTMLDivElement>();
+  const container = useRef<HTMLDivElement | undefined>();
+  const resizableDiv = useRef<HTMLDivElement | undefined>();
+  const otherDiv = useRef<HTMLDivElement | undefined>();
+  const resizer = useRef<HTMLDivElement | undefined>();
   const newPct = useRef(0.5);
   const [isDragging, setIsDragging] = useState(false);
   const isEventDragging = useRef(false);
   const isEventTimeout = useRef(0);
+  const startPos = useRef({ x: 0, y: 0 });
   const [{ widthA, widthB }, setWidths] = useLocalStorage(`resize-layout/${name}`, {
     widthA: options.widthA || '50%',
     widthB: options.widthB || '50%',
   });
 
-  useEventHandler(resizer, 'mousedown', () => {
+  useEventHandler(resizer, 'mousedown', e => {
+    startPos.current.x = e.pageX;
+    startPos.current.y = e.pageY;
     if (isEventTimeout.current) {
       clearTimeout(isEventTimeout.current);
       isEventTimeout.current = 0;
@@ -48,10 +55,10 @@ export function useResizeLayout(
     isEventTimeout.current = setTimeout(() => {
       setIsDragging(true);
       isEventDragging.current = true;
-    }, 250);
+    }, 350);
   });
 
-  useEventHandler({ current: window }, 'mouseup', () => {
+  useEventHandler({ current: typeof window !== 'undefined' ? window : undefined }, 'mouseup', () => {
     const current = container.current;
     if (current && isEventDragging.current) {
       const { x, width } = current.getBoundingClientRect();
@@ -90,7 +97,16 @@ export function useResizeLayout(
     container,
     'mousemove',
     e => {
-      if (isDragging && resizableDiv.current) {
+      if (!isEventDragging.current && isEventTimeout.current) {
+        if (distance(startPos.current.x, startPos.current.y, e.pageX, e.pageY) > 20) {
+          setIsDragging(true);
+          isEventDragging.current = true;
+          clearTimeout(isEventTimeout.current);
+          isEventTimeout.current = 0;
+        }
+      }
+
+      if (isEventDragging.current && resizableDiv.current) {
         const current = container.current;
         if (current) {
           const { x, width } = current.getBoundingClientRect();
