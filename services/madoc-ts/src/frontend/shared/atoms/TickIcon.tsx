27597c16c141edca43c50068dbc88b7c2fc78241
Stephen Fraser <stephen.fraser@digirati.com>
2021-03-18 12:13:20 +0000

    Added plain variation of tick icon

--- services/madoc-ts/src/frontend/shared/atoms/TickIcon.tsx
+++ services/madoc-ts/src/frontend/shared/atoms/TickIcon.tsx
@@ -18,3 +18,21 @@ export const TickIcon = (props: any) => (
     </g>
   </svg>
 );
+
+export const WhiteTickIcon = (props: any) => (
+  <svg
+    width="24px"
+    height="20px"
+    viewBox="0 0 24 12"
+    version="1.1"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    {...props}
+  >
+    <g transform="translate(2, -4)">
+      <polygon
+        fillRule="nonzero"
+        points="7.5 13.5 4 10 2.83333333 11.1666667 7.5 15.8333333 17.5 5.83333333 16.3333333 4.66666667"
+      />
+    </g>
+  </svg>
+);
