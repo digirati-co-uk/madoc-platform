91c85f02d363b82d0a5df6f9c4b31c995fb20e08
Stephen Fraser <stephen.fraser@digirati.com>
2021-03-18 12:11:43 +0000

    Normalised warning and success messages

--- services/madoc-ts/src/frontend/shared/atoms/WarningMessage.tsx
+++ services/madoc-ts/src/frontend/shared/atoms/WarningMessage.tsx
@@ -1,8 +1,13 @@
 import styled from 'styled-components';
 
 export const WarningMessage = styled.div`
-  background: #dd734a;
+  background: #e38627;
   color: #fff;
   width: 100%;
-  padding: 0.5em;
+  padding: 0.5em 1em;
+  line-height: 1.9em;
+
+  a {
+    color: #fff;
+  }
 `;
