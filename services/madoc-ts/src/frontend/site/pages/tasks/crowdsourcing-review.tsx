e49463bce2a93d47c1781223ce4f76755eed9ae5
Stephen Fraser <stephen.fraser@digirati.com>
2021-03-23 12:14:41 +0000

    MAD-674 Back button on all task pages

--- services/madoc-ts/src/frontend/site/pages/tasks/crowdsourcing-review.tsx
+++ services/madoc-ts/src/frontend/site/pages/tasks/crowdsourcing-review.tsx
@@ -1,22 +1,84 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { SubjectSnippet } from '../../../../extensions/tasks/resolvers/subject-resolver';
 import { CrowdsourcingReview } from '../../../../gateway/tasks/crowdsourcing-review';
+import { Button, ButtonRow } from '../../../shared/atoms/Button';
+import { SnippetLarge } from '../../../shared/atoms/SnippetLarge';
+import { LocaleString } from '../../../shared/components/LocaleString';
+import { HrefLink } from '../../../shared/utility/href-link';
+import { useRelativeLinks } from '../../hooks/use-relative-links';
+import { useTaskMetadata } from '../../hooks/use-task-metadata';
 import { TaskContext } from '../loaders/task-loader';
 import { CrowdsourcingManifestReview } from './crowdsourcing-manifest-review';
 import { CrowdsourcingMultiReview } from './crowdsourcing-multi-review';
 
 export const ViewCrowdsourcingReview: React.FC<TaskContext<CrowdsourcingReview>> = ({ task, refetch: refetchTask }) => {
   const reviewType = task.parameters[1] || 'canvas';
+  const { t } = useTranslation();
+  const { subject } = useTaskMetadata<{ subject?: SubjectSnippet }>(task);
+  const createLink = useRelativeLinks();
+  const link = subject
+    ? subject.type === 'manifest'
+      ? createLink({ manifestId: subject.id, taskId: undefined, parentTaskId: undefined })
+      : subject.parent
+      ? createLink({
+          manifestId: subject.parent.id,
+          canvasId: subject.id,
+          taskId: undefined,
+          parentTaskId: undefined,
+          subRoute: 'model',
+        })
+      : null
+    : null;
+
+  const backButton = link ? (
+    <>
+      {subject ? (
+        <SnippetLarge
+          label={
+            subject.parent ? (
+              <LocaleString>{subject.parent.label}</LocaleString>
+            ) : (
+              <LocaleString>{subject.label}</LocaleString>
+            )
+          }
+          linkAs={HrefLink}
+          link={link}
+          subtitle={subject.parent ? <LocaleString>{subject.label}</LocaleString> : undefined}
+          thumbnail={subject.thumbnail || undefined}
+          flat
+          size="sm"
+          smallLabel
+          buttonText="Back to resource"
+        />
+      ) : (
+        <ButtonRow>
+          <Button as={HrefLink} href={link}>
+            {t('Back to resource')}
+          </Button>
+        </ButtonRow>
+      )}
+    </>
+  ) : null;
 
   if (reviewType === 'manifest') {
-    return <CrowdsourcingManifestReview task={task as any} />;
+    return (
+      <>
+        {backButton}
+        <CrowdsourcingManifestReview task={task as any} />;
+      </>
+    );
   }
 
   return (
-    <CrowdsourcingMultiReview
-      task={task}
-      refetch={async () => {
-        await refetchTask();
-      }}
-    />
+    <>
+      {backButton}
+      <CrowdsourcingMultiReview
+        task={task}
+        refetch={async () => {
+          await refetchTask();
+        }}
+      />
+    </>
   );
 };
