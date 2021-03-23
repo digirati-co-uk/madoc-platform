a3050beb2d3257a95214d30499ad8ae29d5337c5
Stephen Fraser <stephen.fraser@digirati.com>
2021-03-23 12:20:26 +0000

    Fixed tooltip on icons

--- services/madoc-ts/src/frontend/site/features/CanvasViewer.tsx
+++ services/madoc-ts/src/frontend/site/features/CanvasViewer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactTooltip from 'react-tooltip';
 import {
   LayoutContainer,
   LayoutContent,
@@ -37,41 +38,44 @@ export const CanvasViewer: React.FC = ({ children }) => {
   });
 
   return (
-    <OuterLayoutContainer style={{ height: '60vh' }}>
-      <LayoutSidebarMenu>
-        {menuItems.map(menuItem => {
-          return (
-            <NavIconContainer
-              key={menuItem.id}
-              $active={menuItem.id === openPanel && isOpen}
-              data-tip={menuItem.label}
-              onClick={() => {
-                if (isOpen && menuItem.id === openPanel) {
-                  setIsOpen(false);
-                } else {
-                  setIsOpen(true);
-                  setOpenPanel(menuItem.id as any);
-                  if (refs.resizableDiv.current) {
-                    refs.resizableDiv.current.scrollTop = 0;
+    <>
+      <OuterLayoutContainer style={{ height: '60vh' }}>
+        <LayoutSidebarMenu>
+          {menuItems.map(menuItem => {
+            return (
+              <NavIconContainer
+                key={menuItem.id}
+                $active={menuItem.id === openPanel && isOpen}
+                data-tip={menuItem.label}
+                onClick={() => {
+                  if (isOpen && menuItem.id === openPanel) {
+                    setIsOpen(false);
+                  } else {
+                    setIsOpen(true);
+                    setOpenPanel(menuItem.id as any);
+                    if (refs.resizableDiv.current) {
+                      refs.resizableDiv.current.scrollTop = 0;
+                    }
                   }
-                }
-              }}
-            >
-              {menuItem.icon}
-            </NavIconContainer>
-          );
-        })}
-      </LayoutSidebarMenu>
-      <LayoutContainer ref={refs.container as any}>
-        {currentMenuItem && isOpen ? (
-          <LayoutSidebar ref={refs.resizableDiv as any} style={{ width: widthB }}>
-            {currentMenuItem.content}
-          </LayoutSidebar>
-        ) : null}
+                }}
+              >
+                {menuItem.icon}
+              </NavIconContainer>
+            );
+          })}
+        </LayoutSidebarMenu>
+        <LayoutContainer ref={refs.container as any}>
+          {currentMenuItem && isOpen ? (
+            <LayoutSidebar ref={refs.resizableDiv as any} style={{ width: widthB }}>
+              {currentMenuItem.content}
+            </LayoutSidebar>
+          ) : null}
 
-        <LayoutHandle ref={refs.resizer as any} onClick={() => setIsOpen(o => !o)} />
-        <LayoutContent>{children}</LayoutContent>
-      </LayoutContainer>
-    </OuterLayoutContainer>
+          <LayoutHandle ref={refs.resizer as any} onClick={() => setIsOpen(o => !o)} />
+          <LayoutContent>{children}</LayoutContent>
+        </LayoutContainer>
+      </OuterLayoutContainer>
+      <ReactTooltip place="right" type="dark" effect="solid" />
+    </>
   );
 };
