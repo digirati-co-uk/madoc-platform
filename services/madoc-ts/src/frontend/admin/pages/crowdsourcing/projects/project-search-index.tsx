67cf70bf5528d57cbadc60c8b4ea1ecc17db347a
Stephen Fraser <stephen.fraser@digirati.com>
2021-04-28 18:42:59 +0100

    MAD-748 Index at collection and project level

---
+++ services/madoc-ts/src/frontend/admin/pages/crowdsourcing/projects/project-search-index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Button } from '../../../../shared/atoms/Button';
+import { useData } from '../../../../shared/hooks/use-data';
+import { useIndexResources } from '../../../hooks/use-index-resource';
+import { ProjectContent } from './project-content';
+
+export const ProjectSearchIndex = () => {
+  const { data: structure } = useData(ProjectContent);
+  const [indexContext, { isLoading, percent }] = useIndexResources(structure?.items || []);
+
+  return (
+    <div>
+      <Button disabled={isLoading} onClick={() => indexContext()}>
+        Index all manifests {isLoading && percent ? ` ${percent}%` : null}
+      </Button>
+    </div>
+  );
+};
