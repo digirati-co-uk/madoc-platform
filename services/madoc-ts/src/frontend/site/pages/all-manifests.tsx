9c745ffb3c2f8fa0a1c38d4bc20705eed4e37c6b
rob.march <rob.march@digirati.com>
2021-05-12 18:49:42 +0100

    MAD-834: Restyle manifests page

--- services/madoc-ts/src/frontend/site/pages/all-manifests.tsx
+++ services/madoc-ts/src/frontend/site/pages/all-manifests.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { LocaleString } from '../../shared/components/LocaleString';
+import {LocaleString, useCreateLocaleString } from '../../shared/components/LocaleString';
 import { Link } from 'react-router-dom';
 import { Pagination } from '../../shared/components/Pagination';
 import { useManifestList } from '../hooks/use-manifest-list';
 import { useRelativeLinks } from '../hooks/use-relative-links';
+import { ImageStripBox } from '../../shared/atoms/ImageStrip';
+import { CroppedImage } from '../../shared/atoms/Images';
+import { SingleLineHeading5, Subheading5 } from '../../shared/atoms/Heading5';
+import { ImageGrid } from '../../shared/atoms/ImageGrid';
 
 export const AllManifests: React.FC = () => {
   const { t } = useTranslation();
   const createLink = useRelativeLinks();
   const { latestData, resolvedData: data } = useManifestList();
+  const createLocaleString = useCreateLocaleString();
 
   return (
     <>
       <h1>{t('All manifests')}</h1>
-      {data?.manifests.map(manifest => (
-        <div key={manifest.id}>
-          <Link to={createLink({ manifestId: manifest.id })}>
-            <LocaleString>{manifest.label}</LocaleString>
+      <ImageGrid>
+        {data?.manifests.map((manifest, idx) => (
+          <Link to={createLink({ manifestId: manifest.id })} key={`${manifest.id}_${idx}`}>
+            <ImageStripBox>
+              <CroppedImage>
+                {manifest.thumbnail ? (
+                  <img
+                    alt={createLocaleString(manifest.label, t('Untitled Manifest'))}
+                    src={manifest.thumbnail}
+                  />
+                ) : null}
+              </CroppedImage>
+              <LocaleString as={SingleLineHeading5}>{manifest.label}</LocaleString>
+              <Subheading5>{t('{{count}} images', { count: manifest.canvasCount })}</Subheading5>
+            </ImageStripBox>
           </Link>
-        </div>
-      ))}
+        ))}
+      </ImageGrid>
       <Pagination
         page={latestData ? latestData.pagination.page : 1}
         totalPages={latestData ? latestData.pagination.totalPages : 1}
