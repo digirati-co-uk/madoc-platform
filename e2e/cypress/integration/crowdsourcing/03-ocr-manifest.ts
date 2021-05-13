370d7d7cb86ccd0c1901410541954b0bf45fd16d
Stephen Fraser <stephen.fraser@digirati.com>
2021-05-13 17:49:17 +0100

    Fixed existing tests

--- e2e/cypress/integration/crowdsourcing/03-ocr-manifest.ts
+++ e2e/cypress/integration/crowdsourcing/03-ocr-manifest.ts
@@ -1,16 +1,19 @@
 /// <reference types="../../types" />
-before(() => {
-  cy.loadSite('wunder-ocr', true);
-});
 
-beforeEach(() => {
-  cy.loadSite('wunder-ocr');
-  cy.visit('/logout');
-});
+describe('OCR manifest', () => {
+  before(() => {
+    cy.loadSite('wunder-ocr', true);
+  });
 
-it('should be able to see the site', () => {
-  // 1. Transcriber makes annotation
-  cy.siteLogin('admin');
-  cy.visit('/s/wunder-ocr/madoc/projects/ocr-correction/manifests/147/c/163/model');
-  cy.waitForReact(1000, '.react-loaded');
+  beforeEach(() => {
+    cy.loadSite('wunder-ocr');
+    cy.visit('/logout');
+  });
+
+  it('should be able to see the site', () => {
+    // 1. Transcriber makes annotation
+    cy.siteLogin('admin');
+    cy.visit('/s/wunder-ocr/madoc/projects/ocr-correction/manifests/147/c/163/model');
+    cy.waitForReact(1000, '.react-loaded');
+  });
 });
