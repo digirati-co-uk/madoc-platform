392fff86bb8cda1502c441a806d94f9bd1f982f0
rob.march <rob.march@digirati.com>
2021-06-11 16:15:37 +0100

    MAD-749: Show preview and delete options for entity selectors

--- services/madoc-ts/src/frontend/shared/caputre-models/VerboseSelector.tsx
+++ services/madoc-ts/src/frontend/shared/caputre-models/VerboseSelector.tsx
@@ -10,6 +10,7 @@ type VerboseSelectorProps = {
 };
 
 export const VerboseSelector: React.FC<VerboseSelectorProps> = ({ selector, readOnly, isTopLevel }) => {
+  const updateSelectorValue = Revisions.useStoreActions(a => a.updateSelector);
   const chooseSelector = Revisions.useStoreActions(a => a.chooseSelector);
   const clearSelector = Revisions.useStoreActions(a => a.clearSelector) as any;
   const currentSelectorId = Revisions.useStoreState(s => s.selector.currentSelectorId);
@@ -21,7 +22,26 @@ export const VerboseSelector: React.FC<VerboseSelectorProps> = ({ selector, read
 
   useEffect(() => componentWillUnmount, [componentWillUnmount]);
 
+  const updateSelector = useCallback(
+    state => {
+      if (selector) {
+        selector.state = state;
+        updateSelectorValue({ selectorId: selector.id, state });
+      }
+    },
+    [selector, updateSelectorValue]
+  );
+
+  if (
+    selector.revisedBy &&
+    selector.revisedBy.length >= 1 &&
+    selector.revisedBy[0].state
+  ) {
+    selector.state = selector.revisedBy[0].state;
+  }
+
   const selectorComponent = useSelectorStatus(selector, {
+    updateSelector: updateSelector,
     chooseSelector: chooseSelector ? (selectorId: string) => chooseSelector({ selectorId }) : undefined,
     clearSelector,
     currentSelectorId: currentSelectorId ? currentSelectorId : undefined,
