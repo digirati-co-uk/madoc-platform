f61daba0670170abc3ae0ffd8eb61f8b99412d3a
Stephen Fraser <stephen.fraser@digirati.com>
2021-06-21 16:39:11 +0100

    Published new public types

--- services/madoc-ts/src/frontend/shared/plugins/public-api.ts
+++ services/madoc-ts/src/frontend/shared/plugins/public-api.ts
@@ -1,4 +1,5 @@
 import { useCollectionList } from '../../site/hooks/use-collection-list';
+import { useRouteContext } from '../../site/hooks/use-route-context';
 import { Button } from '../atoms/Button';
 import { LocaleString } from '../components/LocaleString';
 import { useApi } from '../hooks/use-api';
@@ -19,4 +20,5 @@ export {
   useCollectionList,
   blockConfigFor,
   serverRendererFor,
+  useRouteContext,
 };
