0c2e58dd37d87915b0530521a26ed5a207ab3358
Stephen Fraser <stephen.fraser@digirati.com>
2021-07-07 20:54:57 +0100

    Fixed mapping in JSON

--- services/madoc-ts/src/frontend/admin/pages/crowdsourcing/projects/project-overview.tsx
+++ services/madoc-ts/src/frontend/admin/pages/crowdsourcing/projects/project-overview.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useMutation } from 'react-query';
 import { Button, ButtonRow } from '../../../../shared/atoms/Button';
-import { ProjectStatus } from '../../../../shared/atoms/ProjectStatus';
+import { getStatusMapItem, ProjectStatus } from '../../../../shared/atoms/ProjectStatus';
 import { SubtaskProgress } from '../../../../shared/atoms/SubtaskProgress';
 import { Statistic, StatisticContainer, StatisticLabel, StatisticNumber } from '../../../../shared/atoms/Statistics';
 import { ProjectFull } from '../../../../../types/schemas/project-full';
@@ -55,35 +55,41 @@ export const ProjectOverview: React.FC<{ project: ProjectFull; refetch: () => Pr
             disabled={isLoading || project.status === 4}
             onClick={() => updateStatus(4)}
           >
-            {statusMapping[4] && statusMapping[4].action ? statusMapping[4].action : 'Prepare project'}
+            {getStatusMapItem(statusMapping, 4)?.action
+              ? getStatusMapItem(statusMapping, 4)?.action
+              : 'Prepare project'}
           </Button>
           <Button
             $success={project.status === 0}
             disabled={isLoading || project.status === 0}
             onClick={() => updateStatus(0)}
           >
-            {statusMapping[0] && statusMapping[0].action ? statusMapping[0].action : 'Pause project'}
+            {getStatusMapItem(statusMapping, 0)?.action ? getStatusMapItem(statusMapping, 0)?.action : 'Pause project'}
           </Button>
           <Button
             $success={project.status === 1}
             disabled={isLoading || project.status === 1}
             onClick={() => updateStatus(1)}
           >
-            {statusMapping[1] && statusMapping[1].action ? statusMapping[1].action : 'Resume project'}
+            {getStatusMapItem(statusMapping, 1)?.action ? getStatusMapItem(statusMapping, 1)?.action : 'Resume project'}
           </Button>
           <Button
             $success={project.status === 2}
             disabled={isLoading || project.status === 2}
             onClick={() => updateStatus(2)}
           >
-            {statusMapping[2] && statusMapping[2].action ? statusMapping[2].action : 'Mark as complete'}
+            {getStatusMapItem(statusMapping, 2)?.action
+              ? getStatusMapItem(statusMapping, 2)?.action
+              : 'Mark as complete'}
           </Button>
           <Button
             $success={project.status === 3}
             disabled={isLoading || project.status === 3}
             onClick={() => updateStatus(3)}
           >
-            {statusMapping[3] && statusMapping[3].action ? statusMapping[3].action : 'Archive project'}
+            {getStatusMapItem(statusMapping, 3)?.action
+              ? getStatusMapItem(statusMapping, 3)?.action
+              : 'Archive project'}
           </Button>
         </ButtonRow>
       ) : null}
--- services/madoc-ts/src/frontend/shared/atoms/ProjectStatus.tsx
+++ services/madoc-ts/src/frontend/shared/atoms/ProjectStatus.tsx
@@ -36,7 +36,11 @@ export const ProjectStatusContainer = styled.div<{ $status?: number; $color?: st
   margin: 1em 0;
 `;
 
-export type ProjectStatusMap = { [status: number]: { label?: string; action?: string; info?: string; color?: string } };
+export const getStatusMapItem = (map: any, key: number | string): ProjectStatusMapItem | undefined =>
+  map ? map[Number(key)] : undefined;
+
+export type ProjectStatusMapItem = { label?: string; action?: string; info?: string; color?: string };
+export type ProjectStatusMap = { [status: number]: ProjectStatusMapItem };
 
 export const ProjectStatus: React.FC<{ status?: number; statusMapping?: ProjectStatusMap; template?: string }> = ({
   status: customStatus,
@@ -57,30 +61,30 @@ export const ProjectStatus: React.FC<{ status?: number; statusMapping?: ProjectS
   return (
     <>
       {status === 0 ? (
-        <ProjectStatusContainer $status={0} $color={statusMapping[0]?.color}>
-          {statusMapping[0] && statusMapping[0].info
-            ? statusMapping[0].info
+        <ProjectStatusContainer $status={0} $color={getStatusMapItem(statusMapping, 0)?.color}>
+          {getStatusMapItem(statusMapping, 0)?.info
+            ? getStatusMapItem(statusMapping, 0)?.info
             : t('help__project_paused', { defaultValue: 'This project is paused, only you can see it' })}
         </ProjectStatusContainer>
       ) : null}
       {status === 2 ? (
-        <ProjectStatusContainer $status={2} $color={statusMapping[2]?.color}>
-          {statusMapping[2] && statusMapping[2].info
-            ? statusMapping[2].info
+        <ProjectStatusContainer $status={2} $color={getStatusMapItem(statusMapping, 2)?.color}>
+          {getStatusMapItem(statusMapping, 2)?.info
+            ? getStatusMapItem(statusMapping, 2)?.info
             : t('help__project_published', { defaultValue: 'This project is published' })}
         </ProjectStatusContainer>
       ) : null}
       {status === 3 ? (
-        <ProjectStatusContainer $status={3} $color={statusMapping[3]?.color}>
-          {statusMapping[3] && statusMapping[3].info
-            ? statusMapping[3].info
+        <ProjectStatusContainer $status={3} $color={getStatusMapItem(statusMapping, 3)?.color}>
+          {getStatusMapItem(statusMapping, 3)?.info
+            ? getStatusMapItem(statusMapping, 3)?.info
             : t('help__project_archived', { defaultValue: 'This project is archived, only you can see it' })}
         </ProjectStatusContainer>
       ) : null}
       {status === 4 ? (
-        <ProjectStatusContainer $status={4} $color={statusMapping[4]?.color}>
-          {statusMapping[4] && statusMapping[4].info
-            ? statusMapping[4].info
+        <ProjectStatusContainer $status={4} $color={getStatusMapItem(statusMapping, 4)?.color}>
+          {getStatusMapItem(statusMapping, 4)?.info
+            ? getStatusMapItem(statusMapping, 4)?.info
             : t('help__project_prepare', { defaultValue: 'This project is being prepared, only you can see it' })}
         </ProjectStatusContainer>
       ) : null}
