7aff5cf2620ed1c9f6c396d117959389075df2db
Stephen Fraser <stephen.fraser@digirati.com>
2021-07-14 13:06:43 +0100

    Added event-listeners to keep userLocaleStorage hooks in sync
    When more than one appear in the same tree

--- services/madoc-ts/src/frontend/shared/hooks/use-local-storage.ts
+++ services/madoc-ts/src/frontend/shared/hooks/use-local-storage.ts
@@ -1,6 +1,11 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import mitt from 'mitt';
+
+const lsEmitter = mitt();
 
 export function useLocalStorage<T>(key: string, initialValue?: T) {
+  const lastStoredValue = useRef<string>();
+
   // State to store our value
   // Pass initial state function to useState so logic is only executed once
   const [storedValue, setStoredValue] = useState<T>(() => {
@@ -10,6 +15,9 @@ export function useLocalStorage<T>(key: string, initialValue?: T) {
       }
       // Get from local storage by key
       const item = window.localStorage.getItem(key);
+      if (item) {
+        lastStoredValue.current = item;
+      }
       // Parse stored json or if none return initialValue
       return item ? JSON.parse(item) : initialValue;
     } catch (error) {
@@ -28,14 +36,39 @@ export function useLocalStorage<T>(key: string, initialValue?: T) {
         const valueToStore = value instanceof Function ? value(storedValue) : value;
         // Save state
         setStoredValue(valueToStore);
+        const valueJson = JSON.stringify(valueToStore);
         // Save to local storage
-        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+        window.localStorage.setItem(key, valueJson);
+        // Store last changed value.
+        lastStoredValue.current = valueJson;
+        // Emit change for other hooks.
+        lsEmitter.emit(`change:${key}`, { valueJson });
       }
     } catch (error) {
       // A more advanced implementation would handle the error case
       console.log(error);
     }
   };
+
+  // Detect adjacent hook changes.
+  useEffect(() => {
+    const listener: any = ({ valueJson }: { valueJson: string | null }) => {
+      if (valueJson && valueJson !== lastStoredValue.current) {
+        try {
+          setStoredValue(JSON.parse(valueJson));
+          lastStoredValue.current = valueJson;
+        } catch (e) {
+          // unknown error.
+        }
+      }
+    };
+
+    lsEmitter.on(`change:${key}`, listener);
+
+    return () => {
+      lsEmitter.off(`change:${key}`, listener);
+    };
+  }, [key]);
 
   return [storedValue, setValue] as const;
 }
