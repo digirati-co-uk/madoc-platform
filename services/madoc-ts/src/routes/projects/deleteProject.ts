45823bf4af6083e48abe1d7ec9e5eccd313ca559
rob.march <rob.march@digirati.com>
2021-07-30 17:32:00 +0100

    Delete project

--- services/madoc-ts/src/routes/projects/deleteProject.ts
+++ services/madoc-ts/src/routes/projects/deleteProject.ts
@@ -1,24 +1,52 @@
 import { api } from '../../gateway/api.server';
 import { userWithScope } from '../../utility/user-with-scope';
 import { RouteMiddleware } from '../../types/route-middleware';
-import { deleteProjectMetadata } from '../../database/queries/deletion-queries';
+import {
+  deleteProjectMetadata,
+  deleteProjectNotes, deleteProjectRecord,
+  getProjectAssociates
+} from '../../database/queries/deletion-queries';
+import { DatabasePoolConnectionType} from 'slonik';
+import { deleteCollection } from '../iiif/collections/delete-collection';
+import { buildProjectDeletionSummary } from './delete-project-summary';
 
-export const deleteProject: RouteMiddleware<{ id: number }> = async context => {
+export const deleteProjectEndpoint: RouteMiddleware<{ id: number }> = async context => {
   const { siteId } = userWithScope(context, ['site.admin']);
   const projectId = context.params.id;
 
+  await deleteProject(projectId, siteId, () => context.connection);
+
+  context.response.status = 200;
+};
+
+export async function deleteProject(
+  projectId: number,
+  siteId: number,
+  connection: () => DatabasePoolConnectionType
+) {
   const siteApi = api.asUser({ siteId });
 
-  const deletionSummary = await siteApi.getProjectDeletionSummary(projectId);
+  const deletionSummary = await buildProjectDeletionSummary(projectId, siteId, connection);
+  const { collection_id, capture_model_id } = await connection().one(getProjectAssociates(projectId));
+
+  if (deletionSummary.tasks > 0 || deletionSummary.parentTasks > 0) {
+    await siteApi.batchDeleteTasks({ resourceId: collection_id, subject: `urn:madoc:collection:${collection_id}` });
+  }
 
   if (deletionSummary.search.indexed && deletionSummary.search.id) {
     await siteApi.searchDeleteIIIF(deletionSummary.search.id);
   }
 
+  // Delete capture model
+  await siteApi.deleteCaptureModel(capture_model_id);
+
   // Delete metadata
-  await context.connection.any(deleteProjectMetadata(projectId));
+  await connection().any(deleteProjectMetadata(projectId));
 
-  // TODO actual deletion...
+  // Delete project
+  await connection().any(deleteProjectNotes(projectId));
+  await connection().any(deleteProjectRecord(projectId));
 
-  context.response.status = 200;
-};
+  // Delete project collection
+  await deleteCollection(collection_id, siteId, connection, false);
+}
