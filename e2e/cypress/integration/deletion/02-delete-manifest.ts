68653ba84b57a18bea7f0dc613b93561c0cfb5ba
rob.march <rob.march@digirati.com>
2021-08-03 15:14:50 +0100

    Verify that deletion tests remove resource children

--- e2e/cypress/integration/deletion/02-delete-manifest.ts
+++ e2e/cypress/integration/deletion/02-delete-manifest.ts
@@ -8,12 +8,16 @@ describe('Delete manifest', () => {
   it('Admins should be able to delete manifests', () => {
     cy.siteLogin('admin');
 
-    // Verify there are initially 2 manifests
+    // Verify there are initially 2 manifests and 131 canvases
     cy.visit('/s/default/madoc/admin');
     cy.waitForReact(1000, '.react-loaded');
     cy.get('[data-cy="manifest-count"]').should(
       'have.text',
       '2',
+    );
+    cy.get('[data-cy="canvas-count"]').should(
+      'have.text',
+      '131',
     );
 
     // Delete manifest
@@ -26,12 +30,16 @@ describe('Delete manifest', () => {
     // TODO is this long enough?
     cy.wait(10000);
 
-    // Verify there is now 1 manifest
+    // Verify there is now 1 manifest and 91 canvases
     cy.visit('/s/default/madoc/admin');
     cy.waitForReact(1000, '.react-loaded');
     cy.get('[data-cy="manifest-count"]').should(
       'have.text',
       '1',
+    );
+    cy.get('[data-cy="canvas-count"]').should(
+      'have.text',
+      '91',
     );
   });
 });
--- e2e/cypress/integration/deletion/03-delete-collection.ts
+++ e2e/cypress/integration/deletion/03-delete-collection.ts
@@ -8,15 +8,23 @@ describe('Delete collection', () => {
   it('Admins should be able to delete collections', () => {
     cy.siteLogin('admin');
 
-    // Verify there are initially 2 collections
+    // Verify there are initially 2 collections, 2 manifests, and 131 canvases
     cy.visit('/s/default/madoc/admin');
     cy.waitForReact(1000, '.react-loaded');
     cy.get('[data-cy="collection-count"]').should(
       'have.text',
       '2',
     );
+    cy.get('[data-cy="manifest-count"]').should(
+      'have.text',
+      '2',
+    );
+    cy.get('[data-cy="canvas-count"]').should(
+      'have.text',
+      '131',
+    );
 
-    // Delete manifest
+    // Delete collection
     cy.visit('/s/default/madoc/admin/collections/1/delete');
     cy.waitForReact(1000, '.react-loaded');
     cy.react('Button').contains('Delete collection').should('be.enabled');
@@ -26,12 +34,20 @@ describe('Delete collection', () => {
     // TODO is this long enough?
     cy.wait(10000);
 
-    // Verify there is now 1 collection
+    // Verify there is now 1 collection with no manifests or canvases
     cy.visit('/s/default/madoc/admin');
     cy.waitForReact(1000, '.react-loaded');
     cy.get('[data-cy="collection-count"]').should(
       'have.text',
       '1',
+    );
+    cy.get('[data-cy="manifest-count"]').should(
+      'have.text',
+      '0',
+    );
+    cy.get('[data-cy="canvas-count"]').should(
+      'have.text',
+      '0',
     );
   });
 });
