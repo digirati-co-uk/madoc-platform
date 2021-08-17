749cd9a665bd5d975b005ba503c843b2c5c36d12
rob.march <rob.march@digirati.com>
2021-08-17 18:42:55 +0100

    Regenerate IDs for exported capture model

--- services/madoc-ts/src/routes/projects/export-project-template.ts
+++ services/madoc-ts/src/routes/projects/export-project-template.ts
@@ -5,6 +5,8 @@ import { NotFound } from '../../utility/errors/not-found';
 import { api } from '../../gateway/api.server';
 import { JsonProjectTemplate } from '../../extensions/projects/types';
 import { CaptureModel } from '@capture-models/types';
+import deepmerge from 'deepmerge';
+import { generateId, traverseDocument } from '@capture-models/helpers';
 
 export const exportProjectTemplate: RouteMiddleware<{ id: string }> = async context => {
   const { siteId } = userWithScope(context, []);
@@ -18,6 +20,18 @@ export const exportProjectTemplate: RouteMiddleware<{ id: string }> = async cont
   const project = await userApi.getProject(context.params.id);
   const captureModel: CaptureModel = await userApi.getCaptureModel(project.capture_model_id);
 
+  const newDocument = deepmerge({}, captureModel.document, { clone: true });
+  const updateId = (e: any) => {
+    if (e.id) {
+      e.id = generateId();
+    }
+  };
+  traverseDocument(newDocument, {
+    visitEntity: updateId,
+    visitField: updateId,
+    visitSelector: updateId,
+  });
+
   context.response.body = {
     type: `template-${project.id}-${project.slug}`,
     metadata: {
@@ -28,6 +42,6 @@ export const exportProjectTemplate: RouteMiddleware<{ id: string }> = async cont
     configuration: {
       defaults: project.config,
     },
-    captureModel: captureModel.document
+    captureModel: newDocument,
   } as JsonProjectTemplate;
 };
