0d4f875b64765594ec981f2ba8408be3672a026a
Stephen Fraser <stephen.fraser@digirati.com>
2022-02-17 09:32:24 +0000

    Added alpha page block for canvas panel

--- services/madoc-ts/src/extensions/page-blocks/default-definitions.ts
+++ services/madoc-ts/src/extensions/page-blocks/default-definitions.ts
@@ -1,15 +1,16 @@
+import { CanvasPanelBlock } from '../../frontend/shared/components/CanvasPanelBlock';
 import { Surface } from '../../frontend/shared/layout/Surface';
 import { CrowdSourcingBanner } from '../../frontend/shared/components/CrowdSourcingBanner';
 import { PageBlockDefinition } from './extension';
 import simpleHtmlBlock from './simple-html-block/simple-html-block';
 import currentManifest from './current-manifest-snippet-block';
 import simpleMarkdownBlock from './simple-markdown-block/simple-markdown-block';
-import '../../frontend/shared/layout/Surface';
 
 export const defaultPageBlockDefinitions: PageBlockDefinition<any, any, any, any>[] = [
   simpleHtmlBlock,
   currentManifest,
   (Surface as any)[Symbol.for('slot-model')] as any,
   (CrowdSourcingBanner as any)[Symbol.for('slot-model')] as any,
+  (CanvasPanelBlock as any)[Symbol.for('slot-model')] as any,
   simpleMarkdownBlock,
 ];
