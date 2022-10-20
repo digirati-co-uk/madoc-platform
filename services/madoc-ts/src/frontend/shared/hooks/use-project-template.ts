1e3bf3d0488c1672b63b81556c51543cf194a8a0
Stephen Fraser <stephen.fraser@digirati.com>
2022-10-20 10:49:37 +0100

    Update use-project-template.ts

--- services/madoc-ts/src/frontend/shared/hooks/use-project-template.ts
+++ services/madoc-ts/src/frontend/shared/hooks/use-project-template.ts
@@ -1,4 +1,5 @@
 import { useMemo } from 'react';
+import { useQuery } from 'react-query';
 import { ProjectTemplate } from '../../../extensions/projects/types';
 import { useProjectTemplates } from './use-project-templates';
 
@@ -6,4 +7,26 @@ export function useProjectTemplate(name?: string): ProjectTemplate | undefined {
   const templates = useProjectTemplates();
 
   return useMemo(() => (name ? templates.find(t => t.type === name) : undefined), [name, templates]);
+}
+
+export function useRemoteProjectTemplate(
+  nameOrUrl = 'custom',
+  enabled = false
+): [ProjectTemplate | undefined, boolean, string] {
+  const existing = useProjectTemplate(nameOrUrl);
+  const isRemote = nameOrUrl.startsWith('http');
+  const { data, status } = useQuery(
+    ['get-remote-template', { key: nameOrUrl }],
+    async () => {
+      const fetched = await fetch(nameOrUrl);
+      return fetched.json();
+    },
+    { enabled: isRemote && enabled }
+  );
+
+  if (isRemote) {
+    return [data, true, status];
+  }
+
+  return [existing, false, ''];
 }
