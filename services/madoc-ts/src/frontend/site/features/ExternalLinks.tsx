6a4654f5c2b5b394c03f3a5c59000d7e747e589b
Heather <hkerri200@gmail.com>
2022-10-25 11:30:33 +0100

    external links block

--- services/madoc-ts/src/frontend/site/features/ExternalLinks.tsx
+++ services/madoc-ts/src/frontend/site/features/ExternalLinks.tsx
@@ -1,41 +1,49 @@
 import { blockEditorFor } from '../../../extensions/page-blocks/block-editor-react';
 import React from 'react';
-import { Network } from '@styled-icons/entypo/Network';
 import styled from 'styled-components';
+import { Network } from '@styled-icons/entypo/Network';
 
 const LinkWrapper = styled.div`
   a {
     color: #333333;
-    font-size: 13px;
-
+    font-size: 14px;
+    text-decoration: none;
+    vertical-align: middle;
+    margin: 0 1em;
+    
     :hover {
       color: black;
       text-decoration: underline;
+      cursor: pointer;
     }
   }
+
   svg {
-    color: #002d4b;
-    height: 12px;
-    margin: 0 5px;
+    color: #056db7;
+    height: 18px;
+    margin-right: 8px;
   }
 `;
-export const ExternalLinks: React.FC<{ link?: string; text?: string }> = ({ link, text }) => {
-  // const getIcon = () => {
-  //   if (link?.includes('github')) {
-  //     return <GithubWithCircle />;
-  //   } else if (link?.includes('youtube')) {
-  //     return <YoutubeWithCircle />;
-  //   } else {
-  //     return <Network />;
-  //   }
-  // };
+export const ExternalLinks: React.FC<{
+  links?: {
+    url?: string;
+    text?: string;
+  }[];
+}> = ({ links }) => {
 
   return (
     <LinkWrapper>
-      <a href={link} target="_blank" rel="noreferrer">
-        <Network />
-        {text ? text : link}
-      </a>
+      {links
+        ? links.map((link, i) => {
+            return (
+              link.url && (
+                <a key={i} href={link.url} target="_blank" rel="noreferrer">
+                  <Network /> {link.text ? link.text : link.url}
+                </a>
+              )
+            );
+          })
+        : null}
     </LinkWrapper>
   );
 };
@@ -46,11 +54,29 @@ blockEditorFor(ExternalLinks, {
   anyContext: [],
   requiredContext: [],
   defaultProps: {
-    link: 'hi',
-    text: 'hi',
+    url1: '',
+    text1: '',
+    url2: '',
+    text2: '',
+    url3: '',
+    text3: '',
   },
   editor: {
-    link: { type: 'text-field', label: 'Link' },
-    text: { type: 'text-field', label: 'Link text' },
+    url1: { type: 'text-field', label: 'URL' },
+    text1: { type: 'text-field', label: 'Link text' },
+    url2: { type: 'text-field', label: 'URL' },
+    text2: { type: 'text-field', label: 'Link text' },
+    url3: { type: 'text-field', label: 'URL' },
+    text3: { type: 'text-field', label: 'Link text' },
+  },
+  mapToProps(formInput: any) {
+    const links: {
+      url?: string;
+      text?: string;
+    }[] = [];
+    if (formInput.url1) links.push({ url: formInput.url1, text: formInput.text1 });
+    if (formInput.url2) links.push({ url: formInput.url2, text: formInput.text2 });
+    if (formInput.url3) links.push({ url: formInput.url3, text: formInput.text3 });
+    return { links };
   },
 });
