ba7e5efc46c324e5873c080a7d9d23770ccb18a4
Heather <hkerri200@gmail.com>
2022-11-11 15:45:01 +0000

    add options for manifest pagintation styles

--- services/madoc-ts/src/frontend/site/features/ManifestPagination.tsx
+++ services/madoc-ts/src/frontend/site/features/ManifestPagination.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { blockEditorFor } from '../../../extensions/page-blocks/block-editor-for';
-import { Pagination } from '../../shared/components/Pagination';
+import { Pagination, PaginationNumbered } from '../../shared/components/Pagination';
 import { useLocationQuery } from '../../shared/hooks/use-location-query';
 import { useManifest } from '../hooks/use-manifest';
 import { usePreventCanvasNavigation } from './PreventUsersNavigatingCanvases';
 
-export const ManifestPagination: React.FC = () => {
+export const ManifestPagination: React.FC<{
+  paginationStyle?: boolean;
+  position?: 'flex-end' | 'flex-start' | 'center';
+}> = ({ paginationStyle, position, size }) => {
   const { data } = useManifest();
   const { filter, listing } = useLocationQuery();
   const { showNavigationContent } = usePreventCanvasNavigation();
@@ -17,14 +20,28 @@ export const ManifestPagination: React.FC = () => {
   }
 
   return (
-    <Pagination
-      hash={'listing-header'}
-      pageParam={'m'}
-      page={pagination ? pagination.page : 1}
-      totalPages={pagination ? pagination.totalPages : 1}
-      stale={!pagination}
-      extraQuery={{ filter, listing }}
-    />
+    <>
+      {!paginationStyle ? (
+        <Pagination
+          position={position}
+          hash={'listing-header'}
+          pageParam={'m'}
+          page={pagination ? pagination.page : 1}
+          totalPages={pagination ? pagination.totalPages : 1}
+          stale={!pagination}
+          extraQuery={{ filter, listing }}
+        />
+      ) : (
+        <PaginationNumbered
+          position={position}
+          pageParam={'m'}
+          page={pagination ? pagination.page : 1}
+          totalPages={pagination ? pagination.totalPages : 1}
+          stale={!pagination}
+          extraQuery={{ filter, listing }}
+        />
+      )}
+    </>
   );
 };
 
@@ -33,5 +50,34 @@ blockEditorFor(ManifestPagination, {
   label: 'Manifest pagination',
   anyContext: ['manifest'],
   requiredContext: ['manifest'],
-  editor: {},
+  defaultProps: {
+    paginationStyle: false,
+    position: 'flex-end',
+    width: 'md',
+  },
+  editor: {
+    paginationStyle: {
+      type: 'checkbox-field',
+      inlineLabel: 'Pagination as Numbered?',
+      label: 'Pagination Numbered',
+    },
+    position: {
+      label: 'Position',
+      type: 'dropdown-field',
+      options: [
+        { value: 'flex-start', text: 'Start' },
+        { value: 'center', text: 'Center' },
+        { value: 'flex-end', text: 'End' },
+      ],
+    },
+    width: {
+      label: 'size',
+      type: 'dropdown-field',
+      options: [
+        { value: 'lg', text: 'large' },
+        { value: 'md', text: 'Medium' },
+        { value: 'sm', text: 'Small' },
+      ],
+    },
+  },
 });
