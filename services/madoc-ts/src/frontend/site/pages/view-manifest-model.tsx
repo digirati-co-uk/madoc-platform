5ee29d8ecc3f79232d32eb5c17a0866539cac3ad
Heather <hkerri200@gmail.com>
2023-01-25 16:52:09 +0000

    show canvas viewer inpage with navigation instead of popup for manifest model

--- services/madoc-ts/src/frontend/site/pages/view-manifest-model.tsx
+++ services/madoc-ts/src/frontend/site/pages/view-manifest-model.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import { DisplayBreadcrumbs } from '../../shared/components/Breadcrumbs';
 import { useCurrentUser } from '../../shared/hooks/use-current-user';
 import { Slot } from '../../shared/page-blocks/slot';
-import { ManifestActions } from '../features/ManifestActions';
 import { ManifestCanvasGrid } from '../features/ManifestCanvasGrid';
 import { ManifestHeading } from '../features/ManifestHeading';
 import { ManifestModelEditor } from '../features/ManifestModelEditor';
 import { ManifestPagination } from '../features/ManifestPagination';
 import { ManifestUserNotification } from '../features/ManifestUserNotification';
-import { PrepareCanvasCaptureModel } from '../features/PrepareCanvasCaptureModel';
 import { PrepareManifestsCaptureModel } from '../features/PrepareManifestCaptureModel';
 import { RequiredStatement } from '../features/RequiredStatement';
 import { useManifestTask } from '../hooks/use-manifest-task';
@@ -18,6 +16,13 @@ import { useProjectStatus } from '../hooks/use-project-status';
 import { useRelativeLinks } from '../hooks/use-relative-links';
 import { Navigate } from 'react-router-dom';
 import '../features/ManifestHero';
+import { useRouteContext } from '../hooks/use-route-context';
+import { CustomRouteContext } from '../../shared/page-blocks/slot-context';
+import { useManifestPagination } from '../../shared/components/CanvasNavigationMinimalist';
+import { CanvasViewer } from '../features/CanvasViewer';
+import { StandaloneCanvasViewer } from '../../shared/components/StandaloneCanvasViewer';
+import { HrefLink } from '../../shared/utility/href-link';
+import { Button } from '../../shared/navigation/Button';
 
 export function ViewManifestModel() {
   const createLink = useRelativeLinks();
@@ -26,6 +31,11 @@ export function ViewManifestModel() {
   const user = useCurrentUser(true);
   const { isActive, isPreparing } = useProjectStatus();
   const shadow = useProjectShadowConfiguration();
+
+  const { canvasId } = useRouteContext();
+  const manifestPagination = useManifestPagination();
+  const next = manifestPagination?.nextItem && manifestPagination.nextItem.id;
+  const prev = manifestPagination?.prevItem && manifestPagination.prevItem.id;
 
   const canContribute =
     user &&
@@ -66,16 +76,71 @@ export function ViewManifestModel() {
       <div style={{ display: 'flex' }}>
         <div style={{ flex: 1, minWidth: 0 }}>
           <Slot name="manifest-model-listing-header" id="listing-header">
-            <ManifestPagination />
+            {canvasId ? null : <ManifestPagination />}
           </Slot>
 
           <Slot name="manifest-model-content">
-            <ManifestCanvasGrid popup />
+            {canvasId ? (
+              <CustomRouteContext ctx={{ canvas: canvasId }}>
+                <div
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    padding: '0.5em',
+                    backgroundColor: '#dddddd',
+                  }}
+                >
+                  <Button
+                    as={HrefLink}
+                    to={createLink({
+                      canvasId: '',
+                      subRoute: 'model',
+                    })}
+                  >
+                    View all canvases
+                  </Button>
+                  <div>
+                    {prev && (
+                      <Button
+                        as={HrefLink}
+                        to={
+                          createLink({
+                            canvasId: '',
+                            subRoute: 'model',
+                          }) + `/${prev}`
+                        }
+                      >
+                        Previous
+                      </Button>
+                    )}
+
+                    {next && (
+                      <Button
+                        style={{ marginLeft: '0.5em' }}
+                        as={HrefLink}
+                        to={
+                          createLink({
+                            canvasId: '',
+                            subRoute: 'model',
+                          }) + `/${next}`
+                        }
+                      >
+                        Next
+                      </Button>
+                    )}
+                  </div>
+                </div>
+
+                <CanvasViewer>
+                  <StandaloneCanvasViewer canvasId={canvasId} />
+                </CanvasViewer>
+              </CustomRouteContext>
+            ) : (
+              <ManifestCanvasGrid inPage />
+            )}
           </Slot>
 
-          <Slot name="manifest-model-footer">
-            <ManifestPagination />
-          </Slot>
+          <Slot name="manifest-model-footer">{canvasId ? null : <ManifestPagination />}</Slot>
         </div>
         <div style={{ width: 400, marginLeft: '1em' }}>
           <Slot name="manifest-model-editor" small>
