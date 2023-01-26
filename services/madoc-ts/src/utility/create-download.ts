867022f8c3fdbad86277758c4e650c79d8aac295
Stephen Fraser <stephen.fraser@digirati.com>
2023-01-26 20:44:59 +0000

    Utility updates

--- services/madoc-ts/src/utility/create-download.ts
+++ services/madoc-ts/src/utility/create-download.ts
@@ -1,6 +1,6 @@
 export function createDownload(data: any, fileName: string, fileType: 'text/json') {
   // Create a blob with the data we want to download as a file
-  const blob = new Blob([data], { type: fileType });
+  const blob = data instanceof Blob ? data : new Blob([data], { type: fileType });
   // Create an anchor element and dispatch a click event on it
   // to trigger a download
   const a = document.createElement('a');
