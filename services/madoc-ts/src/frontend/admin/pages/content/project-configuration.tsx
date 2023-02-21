d8f4d10992a6447b22c1a5081c06fbe7d6f06e86
Stephen Fraser <stephen.fraser@digirati.com>
2023-02-21 12:29:57 +0000

    Post-process configuration

--- services/madoc-ts/src/frontend/admin/pages/content/project-configuration.tsx
+++ services/madoc-ts/src/frontend/admin/pages/content/project-configuration.tsx
@@ -4,18 +4,10 @@ import { useMutation } from 'react-query';
 import { useNavigate } from 'react-router-dom';
 import { WidePage } from '../../../shared/layout/WidePage';
 import { EditShorthandCaptureModel } from '../../../shared/capture-models/EditorShorthandCaptureModel';
-import { siteConfigurationModel } from '../../../shared/configuration/site-config';
+import { postProcessConfiguration, siteConfigurationModel } from '../../../shared/configuration/site-config';
 import { useApi } from '../../../shared/hooks/use-api';
 import { apiHooks } from '../../../shared/hooks/use-api-query';
 import { AdminHeader } from '../../molecules/AdminHeader';
-
-function postProcessConfiguration(config: any) {
-  if (config.revisionApprovalsRequired) {
-    config.revisionApprovalsRequired = Number(config.revisionApprovalsRequired);
-  }
-
-  return config;
-}
 
 export const SiteProjectConfiguration: React.FC = () => {
   const { data: value, refetch } = apiHooks.getSiteConfiguration(() => []);
