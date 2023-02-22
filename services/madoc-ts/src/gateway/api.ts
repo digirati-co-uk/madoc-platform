3e7ad10db470a2411c45f5d5050f5e72d439dbed
Matt McGrattan <matt.mcgrattan@digirati.com>
2023-02-22 10:58:18 +0000

    Change API endpoint for indexing on enrichment Change Okra image to one with fixed uWSGI limits and set max connections etc in the sysctls

--- services/madoc-ts/src/gateway/api.ts
+++ services/madoc-ts/src/gateway/api.ts
@@ -1846,7 +1846,7 @@ export class ApiClient {
 
   // NEW SEARCH API.
   async enrichmentIngestResource(request: EnrichmentIndexPayload) {
-    return this.request(`/api/enrichment/internal/madoc/resource/`, {
+    return this.request(`/api/enrichment/resource/`, {
       method: 'POST',
       body: request,
     });
