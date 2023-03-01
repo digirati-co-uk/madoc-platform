49fe6dcefcb3da5c9b3e0b8a0fe88059520cbe6a
Stephen Fraser <stephen.fraser@digirati.com>
2023-03-01 21:57:33 +0000

    NLW-607 Email links alternative

--- services/madoc-ts/src/emails/user-activation-email.tsx
+++ services/madoc-ts/src/emails/user-activation-email.tsx
@@ -27,6 +27,12 @@ export const UserActivationEmail: React.FC<{ installationTitle: string; username
             <Email.MjmlButton background-color="#3579f6" href={resetLink} fontSize={16}>
               Activate your account
             </Email.MjmlButton>
+            <Email.MjmlText color="#333" fontSize={14} lineHeight="1.5">
+              Or copy and paste this link into your browser
+            </Email.MjmlText>
+            <Email.MjmlText color="#333" fontSize={14} lineHeight="1.5">
+              <a href={resetLink}>{resetLink}</a>
+            </Email.MjmlText>
           </Email.MjmlColumn>
         </Email.MjmlSection>
       </Email.MjmlBody>
