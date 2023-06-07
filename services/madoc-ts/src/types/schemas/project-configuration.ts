43109662f47750619b7ed8f15f17144d0fd19147
Heather0K <hkerri200@gmail.com>
2023-06-07 16:10:58 +0100

    merge feature/site-config-sources

--- services/madoc-ts/src/types/schemas/project-configuration.ts
+++ services/madoc-ts/src/types/schemas/project-configuration.ts
@@ -1,8 +1,4 @@
 export type ProjectConfiguration = {
-  _source?: {
-    siteConfig: Array<{ property: string; original: any; override: any }>;
-    staticConfig: Array<{ property: string; original: any; override: any }>;
-  };
   allowCollectionNavigation: boolean;
   allowManifestNavigation: boolean;
   allowCanvasNavigation: boolean;
@@ -102,6 +98,131 @@ export type ProjectConfiguration = {
     manifest?: boolean;
     collection?: boolean;
     canvas?: boolean;
+  };
+
+  // Configuration that's not available from the editor.
+  shadow?: {
+    showCaptureModelOnManifest?: boolean;
+  };
+};
+
+export type ProjectConfigurationNEW = {
+  globalSiteNavigation?: {
+    hideSiteTitle?: boolean;
+    hideProjectsLink?: boolean;
+    hideCollectionsLink?: boolean;
+    hideDashboardLink?: boolean;
+    hidePageNavLinks?: boolean;
+    hideSearchBar?: boolean;
+  };
+  projectPageOptions?: {
+    hideStatistics?: boolean;
+    hideProjectCollectionNavigation?: boolean;
+    hideProjectManifestNavigation?: boolean;
+    hideStartContributing?: boolean;
+    hideSearchButton?: boolean;
+    hideRandomManifest?: boolean;
+    hideRandomCanvas?: boolean;
+    reviewerDashboard?: boolean;
+  };
+  collectionPageOptions?: {
+    hideSearchButton?: false;
+    hideRandomManifest?: false;
+    hideRandomCanvas?: false;
+  };
+  manifestPageOptions?: {
+    hideManifestMetadataOnCanvas?: boolean;
+    hideStartContributing?: boolean;
+    hideOpenInMirador?: boolean;
+    hideSearchButton?: boolean;
+    hideRandomCanvas?: boolean;
+    hideFilterImages?: boolean;
+    directModelPage?: boolean;
+    showIIIFLogo?: boolean;
+    generatePDF?: boolean;
+    coveredImages?: boolean;
+    rectangularImages?: boolean;
+    hideCanvasLabels?: boolean;
+    skipManifestListingPage?: boolean;
+  };
+
+  atlasBackground?: string;
+  miradorCanvasPage?: boolean;
+  universalViewerCanvasPage?: boolean;
+  hideCanvasThumbnailNavigation?: boolean;
+
+  navigation?: {
+    allowCollectionNavigation: boolean;
+    allowManifestNavigation: boolean;
+    allowCanvasNavigation: boolean;
+  };
+
+  searchStrategy?: 'string';
+  searchOptions?: {
+    nonLatinFulltext?: boolean;
+    searchMultipleFields?: boolean;
+    onlyShowManifests?: boolean;
+    showSearchFacetCount?: boolean;
+  };
+
+  contributionMode?: 'transcription' | 'annotation';
+  maxContributionsPerResource: false | number;
+  preventMultipleUserSubmissionsPerResource?: boolean;
+  forkMode?: boolean;
+  claimGranularity: 'canvas' | 'manifest';
+  assigningCanvas?: {
+    randomlyAssignCanvas?: boolean;
+    priorityRandomness?: boolean;
+  };
+  randomCanvas?: boolean;
+  defaultEditorOrientation: 'vertical' | 'horizontal';
+  contributionPanelOptions?: {
+    hideViewerControls?: boolean;
+    enableRotation?: boolean;
+    fixedTranscriptionBar?: boolean;
+    disableSaveForLater?: boolean;
+    allowPersonalNotes?: boolean;
+  };
+  contributionWarningTime: false | number;
+  shortExpiryTime?: string;
+  longExpiryTime?: string;
+
+  submissionOptions?: {
+    disablePreview?: boolean;
+    disableNextCanvas?: boolean;
+    preventContributionAfterManifestUnassign?: boolean;
+    preventContributionAfterRejection?: boolean;
+    preventContributionAfterSubmission?: boolean;
+  };
+
+  modelPageShowAnnotations?: 'always' | 'when-open' | 'highlighted';
+  modelPageShowDocument?: 'always' | 'when-open' | 'highlighted';
+  canvasPageShowAnnotations?: 'always' | 'when-open' | 'highlighted';
+  canvasPageShowDocument?: 'always' | 'when-open' | 'highlighted';
+
+  randomlyAssignReviewer: boolean;
+  adminsAreReviewers: boolean;
+  manuallyAssignedReviewer?: number | null;
+  revisionApprovalsRequired: number;
+  reviewOptions?: {
+    allowMerging?: boolean;
+    enableAutoReview?: boolean;
+  };
+  hideCompletedResources: boolean;
+  allowSubmissionsWhenCanvasComplete: boolean;
+
+  //other
+  skipAutomaticOCRImport?: boolean;
+  metadataSuggestions?: {
+    manifest?: boolean;
+    collection?: boolean;
+    canvas?: boolean;
+  };
+  activityStreams?: {
+    manifest?: boolean;
+    canvas?: boolean;
+    curated?: boolean;
+    published?: boolean;
   };
 
   // Configuration that's not available from the editor.
