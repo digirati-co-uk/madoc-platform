2b85d1300ad58f1d8eb2a6094967975493530d8c
Heather0K <hkerri200@gmail.com>
2023-06-08 11:09:05 +0100

    accordians

--- services/madoc-ts/src/frontend/admin/pages/crowdsourcing/projects/project-configuration-new.tsx
+++ services/madoc-ts/src/frontend/admin/pages/crowdsourcing/projects/project-configuration-new.tsx
@@ -19,7 +19,8 @@ import { useShortMessage } from '../../../../shared/hooks/use-short-message';
 import { serverRendererFor } from '../../../../shared/plugins/external/server-renderer-for';
 import { InfoMessage } from '../../../../shared/callouts/InfoMessage';
 import { Heading2 } from '../../../../shared/typography/Heading2';
-import {Accordion} from "../../../../shared/atoms/Accordion";
+import { Accordion, AccordionItem, AccordionContainer } from '../../../../shared/navigation/Accordion';
+import { BugIcon } from '../../../../shared/icons/BugIcon';
 
 export const ProjectConfigurationNew: React.FC = () => {
   const params = useParams() as { id: string };
@@ -45,54 +46,87 @@ export const ProjectConfigurationNew: React.FC = () => {
     return null;
   }
   return (
-    <div>
+    <div style={{ flex: 1, marginBottom: '2em' }}>
       <InfoMessage>🚧 New Configuration WIP 🚧</InfoMessage>
 
-      <Heading2>Interface</Heading2>
+      <AccordionContainer>
+        <AccordionItem
+          large
+          label="Search configuration"
+          description="With description"
+          icon={<BugIcon />}
+          maxHeight={false}
+        >
+          <div style={{ height: '1800px' }}>
+            <EditShorthandCaptureModel
+              key={updatedAt}
+              searchLabel={t('Search configuration')}
+              immutableFields={projectTemplate?.configuration?.immutable}
+              data={projectConfiguration}
+              template={ProjectConfigInterface}
+            />
+          </div>
+        </AccordionItem>
 
-      <EditShorthandCaptureModel
-        key={updatedAt}
-        searchLabel={t('Search configuration')}
-        immutableFields={projectTemplate?.configuration?.immutable}
-        data={projectConfiguration}
-        template={ProjectConfigInterface}
-      />
+        <AccordionItem large label="Interface" description="With description" icon={<BugIcon />} maxHeight={false}>
+          <div style={{ height: '1750px' }}>
+            <EditShorthandCaptureModel
+              key={updatedAt}
+              immutableFields={projectTemplate?.configuration?.immutable}
+              data={projectConfiguration}
+              template={ProjectConfigInterface}
+            />
+          </div>
+        </AccordionItem>
 
-      <Heading2>Search & browse</Heading2>
-      <EditShorthandCaptureModel
-        key={updatedAt}
-        searchLabel={t('Search configuration')}
-        immutableFields={projectTemplate?.configuration?.immutable}
-        data={projectConfiguration}
-        template={ProjectConfigSearch}
-      />
+        <AccordionItem
+          large
+          label="Search & browse"
+          description="With description"
+          icon={<BugIcon />}
+          maxHeight={false}
+        >
+          <div style={{ height: '350px' }}>
+            <EditShorthandCaptureModel
+              key={updatedAt}
+              immutableFields={projectTemplate?.configuration?.immutable}
+              data={projectConfiguration}
+              template={ProjectConfigSearch}
+            />
+          </div>
+        </AccordionItem>
 
-      <Heading2>Contributions</Heading2>
-      <EditShorthandCaptureModel
-        key={updatedAt}
-        searchLabel={t('Search configuration')}
-        immutableFields={projectTemplate?.configuration?.immutable}
-        data={projectConfiguration}
-        template={ProjectConfigContributions}
-      />
+        {/*<AccordionItem large label="Contributions" description="With description" icon={<BugIcon />} maxHeight={false}>*/}
+        {/*    <EditShorthandCaptureModel*/}
+        {/*    key={updatedAt}*/}
+        {/*    immutableFields={projectTemplate?.configuration?.immutable}*/}
+        {/*    data={projectConfiguration}*/}
+        {/*    template={ProjectConfigContributions}*/}
+        {/*  />*/}
+        {/*</AccordionItem>*/}
 
-      <Heading2>Review process</Heading2>
-      <EditShorthandCaptureModel
-        key={updatedAt}
-        searchLabel={t('Search configuration')}
-        immutableFields={projectTemplate?.configuration?.immutable}
-        data={projectConfiguration}
-        template={ProjectConfigReview}
-      />
+        <AccordionItem large label="Review process" description="With description" icon={<BugIcon />} maxHeight={false}>
+            <div style={{ height: '720px' }}>
+          <EditShorthandCaptureModel
+            key={updatedAt}
+            immutableFields={projectTemplate?.configuration?.immutable}
+            data={projectConfiguration}
+            template={ProjectConfigReview}
+          />
+            </div>
+        </AccordionItem>
 
-      <Heading2>Other</Heading2>
-      <EditShorthandCaptureModel
-        key={updatedAt}
-        searchLabel={t('Search configuration')}
-        immutableFields={projectTemplate?.configuration?.immutable}
-        data={projectConfiguration}
-        template={ProjectConfigOther}
-      />
+        <AccordionItem large label="Other" description="With description" icon={<BugIcon />} maxHeight={false}>
+            <div style={{ height: '650px' }}>
+            <EditShorthandCaptureModel
+            key={updatedAt}
+            immutableFields={projectTemplate?.configuration?.immutable}
+            data={projectConfiguration}
+            template={ProjectConfigOther}
+          />
+            </div>
+        </AccordionItem>
+      </AccordionContainer>
     </div>
   );
 };
