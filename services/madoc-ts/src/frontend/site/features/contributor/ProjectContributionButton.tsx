8117d84998b15c9012c5b502148e6b7be5c6656f
Stephen Fraser <stephen.fraser@digirati.com>
2023-07-31 11:46:15 +0100

    Project contribution (new hook + tailwind)

--- services/madoc-ts/src/frontend/site/features/contributor/ProjectContributionButton.tsx
+++ services/madoc-ts/src/frontend/site/features/contributor/ProjectContributionButton.tsx
@@ -1,81 +1,46 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { blockEditorFor } from '../../../../extensions/page-blocks/block-editor-for';
-import { parseUrn } from '../../../../utility/parse-urn';
+import { useRecentUserTasks } from '../../../shared/hooks/use-recent-user-tasks';
 import { SmallButton } from '../../../shared/navigation/Button';
 import { CSSThirdGrid } from '../../../shared/layout/Grid';
 import { Heading3 } from '../../../shared/typography/Heading3';
 import { ContinueTaskDisplay } from '../../../shared/components/ContinueTaskDisplay';
-import { useContributorTasks } from '../../../shared/hooks/use-contributor-tasks';
-import { firstNTasksWithUniqueSubjects } from '../../../shared/utility/first-n-tasks-with-unique-subjects';
 import { HrefLink } from '../../../shared/utility/href-link';
 import { useProject } from '../../hooks/use-project';
 import { useProjectShadowConfiguration } from '../../hooks/use-project-shadow-configuration';
 import { useProjectStatus } from '../../hooks/use-project-status';
 import { useRelativeLinks } from '../../hooks/use-relative-links';
-import { useSiteConfiguration } from '../SiteConfigurationContext';
 
 export const ProjectContributionButton: React.FC = () => {
   const { t } = useTranslation();
   const { data: project } = useProject();
   const { isActive } = useProjectStatus();
   const createLink = useRelativeLinks();
-  const {
-    project: { contributionMode, claimGranularity },
-  } = useSiteConfiguration();
-  const contributorTasks = useContributorTasks({ rootTaskId: project?.task_id }, !!project);
   const { showCaptureModelOnManifest } = useProjectShadowConfiguration();
 
-  const currentTasks = contributorTasks?.drafts.tasks;
-  const tasksInReview = contributorTasks?.reviews.tasks;
+  const recentTasks = useRecentUserTasks(3);
 
   if (!project || !isActive) {
     return null;
   }
 
-  const taskComponents = [];
-
-  if (currentTasks && currentTasks.length) {
-    const firstThree = firstNTasksWithUniqueSubjects(currentTasks, 3);
-
-    for (const task of firstThree) {
-      taskComponents.push(
-        <div key={task.id}>
-          <ContinueTaskDisplay task={task} manifestModel={showCaptureModelOnManifest} />
-        </div>
-      );
-    }
-  }
-
-  if (
-    contributionMode !== 'transcription' &&
-    claimGranularity !== 'manifest' &&
-    tasksInReview &&
-    tasksInReview.length &&
-    taskComponents.length < 3
-  ) {
-    const firstThree = firstNTasksWithUniqueSubjects(tasksInReview, 3 - taskComponents.length);
-
-    for (const task of firstThree) {
-      const parsed = parseUrn(task.subject);
-      if (parsed && parsed.type === 'canvas') {
-        taskComponents.push(
-          <div key={task.id}>
-            <ContinueTaskDisplay task={task} next manifestModel={showCaptureModelOnManifest} />
-          </div>
-        );
-      }
-    }
-  }
-
-  if (taskComponents.length === 0) {
+  if (!recentTasks.length) {
     return null;
   }
 
   return (
     <div>
       <Heading3 $margin>{t('Continue where you left off')}</Heading3>
-      <CSSThirdGrid $justify="flex-start">{taskComponents}</CSSThirdGrid>
+      <CSSThirdGrid $justify="flex-start">
+        {recentTasks.map(task => {
+          return (
+            <div key={task.task.id}>
+              <ContinueTaskDisplay task={task.task} next={task.next} manifestModel={showCaptureModelOnManifest} />
+            </div>
+          );
+        })}
+      </CSSThirdGrid>
       <SmallButton
         as={HrefLink}
         href={createLink({ projectId: project.id, subRoute: 'tasks', query: { type: 'crowdsourcing-task' } })}
