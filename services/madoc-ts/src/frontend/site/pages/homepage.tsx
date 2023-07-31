03203afc7e92c0cc72f9a5168094038a59e99799
Stephen Fraser <stephen.fraser@digirati.com>
2023-07-31 11:47:23 +0100

    Added test blocks to homepage

--- services/madoc-ts/src/frontend/site/pages/homepage.tsx
+++ services/madoc-ts/src/frontend/site/pages/homepage.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import { AvailableBlocks } from '../../shared/page-blocks/available-blocks';
 import { Slot } from '../../shared/page-blocks/slot';
-import { Test } from '../../tailwind/components/Test';
+import { HeroCentered } from '../../tailwind/blocks/HeroCentered';
+import { HeroHyper } from '../../tailwind/blocks/HeroHyper';
 import { DefaultHomepage } from '../features/DefaultHomepage';
 import { StaticPage } from '../features/StaticPage';
 
 export const Homepage = () => {
   return (
     <StaticPage title="Homepage">
-      <Test />
       <Slot name="homepage-header" />
       <Slot name="homepage-main">
         <DefaultHomepage />
+        <AvailableBlocks>
+          <HeroCentered title="" image={null} description="" />
+          <HeroHyper title="" image={null} description="" />
+        </AvailableBlocks>
       </Slot>
       <Slot name="homepage-footer" />
     </StaticPage>
