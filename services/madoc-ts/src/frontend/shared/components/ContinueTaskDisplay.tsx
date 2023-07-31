ae34436efccc9b2974c64fdb23628bbfb330d27e
Stephen Fraser <stephen.fraser@digirati.com>
2023-07-31 11:44:06 +0100

    Added hook for continue contribution

--- services/madoc-ts/src/frontend/shared/components/ContinueTaskDisplay.tsx
+++ services/madoc-ts/src/frontend/shared/components/ContinueTaskDisplay.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { SubjectSnippet as SubjectSnippetType } from '../../../extensions/tasks/resolvers/subject-resolver';
 import { BaseTask } from '../../../gateway/tasks/base-task';
-import { useRelativeLinks } from '../../site/hooks/use-relative-links';
 import { useTaskMetadata } from '../../site/hooks/use-task-metadata';
+import { useContinueContribution } from '../hooks/use-continue-contribution';
 import { PrimaryButtonLink } from '../navigation/Button';
 import { SnippetLarge } from '../atoms/SnippetLarge';
 import { useCreateLocaleString } from './LocaleString';
@@ -18,21 +18,9 @@ export function ContinueTaskDisplay({
   manifestModel?: boolean;
 }) {
   const { subject } = useTaskMetadata<{ subject?: SubjectSnippetType }>(task);
+  const link = useContinueContribution(task, manifestModel);
   const createLocalString = useCreateLocaleString();
-  const createLink = useRelativeLinks();
   const { t } = useTranslation();
-
-  const link = subject
-    ? subject.type === 'manifest'
-      ? createLink({ manifestId: subject.id, subRoute: manifestModel ? 'model' : '' })
-      : subject.type === 'canvas'
-      ? createLink({
-          canvasId: subject.id,
-          manifestId: subject.parent?.id,
-          subRoute: 'model',
-        })
-      : undefined
-    : undefined;
 
   if (!subject || !link) {
     return null;
