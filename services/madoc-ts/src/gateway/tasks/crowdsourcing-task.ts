65ff7f379b536d9a85533d4d94673c865b4b97ce
Stephen Fraser <stephen.fraser@digirati.com>
2023-08-09 13:34:57 +0100

    Fix notification/rejection loop

--- services/madoc-ts/src/gateway/tasks/crowdsourcing-task.ts
+++ services/madoc-ts/src/gateway/tasks/crowdsourcing-task.ts
@@ -139,11 +139,12 @@ export const jobHandler = async (name: string, taskId: string, api: ApiClient) =
         if (revision) {
           const revisionRequest = await api.crowdsourcing.getCaptureModelRevision(revision);
           if (revisionRequest) {
+            // @todo mark revision _request_ as rejected.
             // await api.deleteCaptureModelRevision(revisionRequest);
-            await api.updateTask(task.id, {
-              status: -1,
-              status_text: `rejected`,
-            });
+            // await api.updateTask(task.id, {
+            //   status: -1,
+            //   status_text: `rejected`,
+            // });
           }
         }
         if (task.parent_task) {
