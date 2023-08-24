0e44c9d2a5393ccb5e61a7f233e869fd34ef6337
Heather0K <hkerri200@gmail.com>
2023-08-24 11:29:06 +0100

    tidy up and check for selector value as well

--- services/madoc-ts/src/frontend/shared/capture-models/new/hooks/use-resolved-dependant.ts
+++ services/madoc-ts/src/frontend/shared/capture-models/new/hooks/use-resolved-dependant.ts
@@ -1,41 +1,23 @@
 import { BaseField } from '../../types/field-types';
 import { Revisions } from '../../editor/stores/revisions';
+import { isEntityEmpty } from '../../utility/is-entity-empty';
 
 export function useResolvedDependant(entityOrField: BaseField) {
   const currentRevision = Revisions.useStoreState(s => s.currentRevision);
   const revisionDocument = currentRevision?.document;
-  const fieldsToValidate: any[] = [];
 
-  if (entityOrField.dependant) {
-    const dependantField = currentRevision?.document.properties[entityOrField.dependant];
-
-    if (revisionDocument) {
-      if (dependantField && dependantField.length) {
-        for (const singleFieldOrEntity of dependantField) {
-          if (singleFieldOrEntity.properties) {
-            const properties = Object.keys(singleFieldOrEntity.properties);
-            for (const property of properties) {
-              const subFieldOrEntity = singleFieldOrEntity.properties[property];
-              if (subFieldOrEntity && subFieldOrEntity.length) {
-                for (const singleSubFieldOrEntity of subFieldOrEntity) {
-                  fieldsToValidate.push(singleSubFieldOrEntity);
-                }
-              }
-            }
-          } else {
-            fieldsToValidate.push(singleFieldOrEntity);
-          }
-        }
+  if (entityOrField.dependant && revisionDocument) {
+    const dependantField = revisionDocument.properties[entityOrField.dependant][0];
+    if (dependantField) {
+      if (dependantField.type === 'entity') {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        const isEmpty = isEntityEmpty(dependantField);
+        return !isEmpty;
       }
-    }
-
-    if (fieldsToValidate.length) {
-      for (const field of fieldsToValidate) {
-        if (!field.value || field.value === '') {
-          return false;
-        }
-      }
-      return true;
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      return !(!dependantField.value || dependantField.value === '') || !!dependantField.selector?.state;
     }
     return true;
   }
