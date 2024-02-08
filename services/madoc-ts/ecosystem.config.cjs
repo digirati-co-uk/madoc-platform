d8501a8a053ab9a2dfb4eea322c7972d07dd2bf3
Stephen Fraser <stephen.fraser@digirati.com>
2024-02-08 12:53:44 +0000

    Memory changes to PM2

--- services/madoc-ts/ecosystem.config.cjs
+++ services/madoc-ts/ecosystem.config.cjs
@@ -2,50 +2,50 @@
 module.exports = {
   apps: [
     {
-      name: "server",
-      script: "entrypoint/server.cjs",
+      name: 'server',
+      script: 'entrypoint/server.cjs',
       instances: 1,
-      instance_var: "NODE_APP_INSTANCE",
-      exec_mode: "cluster",
+      instance_var: 'NODE_APP_INSTANCE',
+      exec_mode: 'cluster',
       autorestart: true,
       watch: false,
-      max_memory_restart: "300M",
+      max_memory_restart: '640M',
       env: {
-        NODE_ENV: process.env.NODE_ENV
+        NODE_ENV: process.env.NODE_ENV,
       },
       env_production: {
-        NODE_ENV: "production"
-      }
+        NODE_ENV: 'production',
+      },
     },
     {
-      name: "queue",
-      script: "entrypoint/producer.cjs",
+      name: 'queue',
+      script: 'entrypoint/producer.cjs',
       instances: 2,
-      instance_var: "NODE_APP_INSTANCE",
-      exec_mode: "cluster",
+      instance_var: 'NODE_APP_INSTANCE',
+      exec_mode: 'cluster',
       autorestart: true,
-      max_memory_restart: "300M",
-      watch: false
+      max_memory_restart: '180M',
+      watch: false,
     },
     {
-      name: "scheduler",
-      script: "entrypoint/scheduler.cjs",
+      name: 'scheduler',
+      script: 'entrypoint/scheduler.cjs',
       instances: 1,
-      exec_mode: "cluster",
-      instance_var: "NODE_APP_INSTANCE",
+      exec_mode: 'cluster',
+      instance_var: 'NODE_APP_INSTANCE',
       autorestart: true,
       watch: false,
-      max_memory_restart: "300M"
+      max_memory_restart: '100M',
     },
     {
-      name: "auth",
-      script: "entrypoint/auth.cjs",
-      max_memory_restart: "300M",
+      name: 'auth',
+      script: 'entrypoint/auth.cjs',
+      max_memory_restart: '100M',
       instances: 1,
-      exec_mode: "cluster",
-      instance_var: "NODE_APP_INSTANCE",
+      exec_mode: 'cluster',
+      instance_var: 'NODE_APP_INSTANCE',
       autorestart: true,
-      watch: false
-    }
-  ]
+      watch: false,
+    },
+  ],
 };
