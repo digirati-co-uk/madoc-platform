4922f39cc4309740e0a70ed9b1d8211e90716287
Heather0K <hkerri200@gmail.com>
2024-03-04 16:53:24 +0000

    create topic-search

---
+++ services/madoc-ts/src/frontend/site/features/TopicPageResults.tsx
@@ -0,0 +1,130 @@
+import React, { useState } from 'react';
+import { useSearchQuery } from '../hooks/use-search-query';
+import { blockEditorFor } from '../../../extensions/page-blocks/block-editor-for';
+import { SearchResult } from '../../../types/search';
+import { useSearch } from '../hooks/use-search';
+import { ResultsContainer, SearchItem, TotalResults } from '../../shared/components/SearchResults';
+import { ImageGrid } from '../../shared/atoms/ImageGrid';
+import { LoadingBlock } from '../../shared/callouts/LoadingBlock';
+import { useTranslation } from 'react-i18next';
+import { SimpleDropdown } from '../../shared/atoms/SimpleDropdown';
+import { Button, ButtonIcon } from '../../shared/navigation/Button';
+import { GridIcon } from '../../shared/icons/GridIcon';
+import { ListIcon } from '../../shared/icons/ListIcon';
+import { useTopicSearch } from '../hooks/use-topic-search';
+import { useRouteContext } from '../hooks/use-route-context';
+
+interface TopicPageResultsProps {
+  background?: string;
+  grid?: boolean;
+  snippet?: boolean;
+  textColor?: string;
+  cardBorder?: string;
+  imageStyle?: string;
+}
+
+export const TopicPageResults: React.FC<TopicPageResultsProps> = ({
+  snippet,
+  cardBorder,
+  textColor,
+  background,
+  imageStyle,
+}) => {
+  const { t } = useTranslation();
+  const [{ resolvedData: searchResponse, latestData }, displayFacets, isLoading] = useTopicSearch();
+  const { rawQuery, page, fulltext } = useSearchQuery();
+  const searchResults = searchResponse ? searchResponse.results : [];
+
+  const [resultsView, setResultsView] = useState('list');
+  if (!searchResults) {
+    return null;
+  }
+  return isLoading || !searchResponse ? (
+    <LoadingBlock />
+  ) : (
+    <>
+      <div style={{ justifyContent: 'space-between', display: 'flex' }}>
+        <TotalResults>
+          {t('Found {{count}} results', {
+            count: searchResponse && searchResponse.pagination ? searchResponse.pagination.totalResults : 0,
+          })}
+        </TotalResults>
+
+        <SimpleDropdown buttonText={<ButtonIcon>{resultsView === 'list' ? <ListIcon /> : <GridIcon />}</ButtonIcon>}>
+          <Button
+            $link
+            style={{ display: 'flex', justifyContent: 'space-evenly' }}
+            onClick={() => {
+              setResultsView('list');
+            }}
+          >
+            List
+            <ButtonIcon>
+              <ListIcon />
+            </ButtonIcon>
+          </Button>
+
+          <Button
+            $link
+            style={{ display: 'flex', justifyContent: 'space-evenly' }}
+            onClick={() => {
+              setResultsView('grid');
+            }}
+          >
+            Grid
+            <ButtonIcon>
+              <GridIcon />
+            </ButtonIcon>
+          </Button>
+        </SimpleDropdown>
+      </div>
+      <ResultsContainer $isFetching={isLoading}>
+        <ImageGrid data-view-list={resultsView === 'list'} $size={'small'}>
+          {searchResults.map((result: SearchResult, index: number) => {
+            return result ? (
+              <SearchItem
+                result={result}
+                key={`${index}__${result.resource_id}`}
+                search={fulltext}
+                background={background}
+                border={cardBorder}
+                textColor={textColor}
+                list={resultsView === 'list'}
+                hideSnippet={snippet}
+                imageStyle={imageStyle}
+              />
+            ) : null;
+          })}
+        </ImageGrid>
+      </ResultsContainer>
+    </>
+  );
+};
+
+blockEditorFor(TopicPageResults, {
+  label: 'topic Page Results',
+  type: 'default.TopicPageResults',
+  anyContext: ['topic', 'topicType'],
+  requiredContext: ['page'],
+  defaultProps: {
+    background: '',
+    snippet: '',
+    textColor: '',
+    cardBorder: '',
+    imageStyle: 'fit',
+  },
+  editor: {
+    snippet: { type: 'checkbox-field', label: 'Snippet', inlineLabel: 'Hide snippet?' },
+    background: { label: 'Card background color', type: 'color-field' },
+    textColor: { label: 'Card text color', type: 'color-field' },
+    cardBorder: { label: 'Card border', type: 'color-field' },
+    imageStyle: {
+      label: 'Image Style',
+      type: 'dropdown-field',
+      options: [
+        { value: 'covered', text: 'covered' },
+        { value: 'fit', text: 'fit' },
+      ],
+    },
+  },
+});
--- services/madoc-ts/src/frontend/site/hooks/use-topic-search.ts
+++ services/madoc-ts/src/frontend/site/hooks/use-topic-search.ts
@@ -1,5 +1,5 @@
 import { InternationalString } from '@iiif/presentation-3';
-import { useEffect, useMemo } from 'react';
+import { useMemo } from 'react';
 import { FacetConfig } from '../../shared/components/MetadataFacetEditor';
 import { apiHooks, paginatedApiHooks } from '../../shared/hooks/use-api-query';
 import { useSiteConfiguration } from '../features/SiteConfigurationContext';
@@ -10,21 +10,14 @@ function normalizeDotKey(key: string) {
   return key.startsWith('metadata.') ? key.slice('metadata.'.length).toLowerCase() : key.toLowerCase();
 }
 
-export function useSearch() {
-  const { projectId, collectionId, manifestId, topic } = useRouteContext();
+export function useTopicSearch() {
+  const { topic } = useRouteContext();
   const { fulltext, appliedFacets, page, rscType } = useSearchQuery();
   const {
     project: { searchStrategy, claimGranularity, searchOptions },
   } = useSiteConfiguration();
   const { searchMultipleFields, nonLatinFulltext, onlyShowManifests } = searchOptions || {};
   const searchFacetConfig = apiHooks.getSiteSearchFacetConfiguration(() => []);
-
-  useEffect(() => {
-    if ((topic && !appliedFacets.length) || topic) {
-      appliedFacets.push({ k: 'entity', v: topic, t: 'entity' });
-    }
-    return;
-  }, [appliedFacets, topic]);
 
   const [facetsToRequest, facetDisplayOrder, facetIdMap] = useMemo(() => {
     const facets = !topic && searchFacetConfig.data ? searchFacetConfig.data.facets : [];
@@ -61,24 +54,13 @@ export function useSearch() {
         };
   });
 
-  const searchQFacets = appliedFacets.map(facet => ({
-    type: facet.t,
-    subtype: facet.k,
-    value: facet.v,
-  }));
-
-  const facets = topic ? topicFacets : searchQFacets;
-  const waitForTopic = topic ? !!facets.length : true;
   const searchResults = paginatedApiHooks.getSiteSearchQuery(
     () => [
       {
-        projectId,
-        collectionId,
-        manifestId,
         fulltext: fulltext,
         facet_fields: facetsToRequest.length ? facetsToRequest : undefined,
         //  @todo stringify facets.
-        facets: facets,
+        facets: [...topicFacets, { type: 'entity', group_id: topic || '' }],
         resource_type: rscType,
         facet_on_manifests: true,
         search_type: searchStrategy as any,
@@ -92,52 +74,18 @@ export function useSearch() {
     {
       enabled:
         !searchFacetConfig.isLoading &&
-        waitForTopic &&
-        (!!facetsToRequest.length ||
-          !!fulltext ||
-          fulltext === '' ||
-          !!facets ||
-          !!rscType ||
-          collectionId ||
-          manifestId ||
-          projectId ||
-          !!topic),
+        (!!facetsToRequest.length || !!fulltext || fulltext === '' || !!topicFacets || !!rscType || !!topic),
       staleTime: 0,
     }
   );
 
   const displayFacets = useMemo(() => {
-    // We need to display the facets. We have two lists.
-    // mappedFacets:
-    // {
-    //   title: {id: "8159f355-3ca7-4f25-8100-80919b20a064", keys: ['metadata.Title'], values: Array(0), label: {…}}
-    // }
-    //
-    // searchResponse.data.facets
-    // {
-    //   metadata: {
-    //     title: {
-    //       'Wunder der Vererbung': 1,
-    //     }
-    //   }
-    // }
-    //
-    // And we want to format this into a single list. The following rules applied:
-    // - If mapped facets contains multiple keys, they should be merged together.
-    // - If the values list is not empty, only those values should be used.
-    // - The internationalised label should be used
     const mappedSearchResponseMetadata: {
       [key: string]: Array<{ key: string; value: string; count: number; configuration?: FacetConfig }>;
     } = {};
 
     const metadataFacets = searchResults.resolvedData?.facets?.metadata || {};
     const entityFacets = searchResults.resolvedData?.facets?.entity || {};
-
-    // const facetType = { ...metadataFacets, ...entityFacets };
-    const showAllFacets = facetDisplayOrder.length === 0;
-    // for (const facet of Object.keys(facetType)) {
-    //   const values = Object.keys(facetType[facet]);
-    //   const normalisedKey = facet.toLowerCase();
 
     for (const mfacet of Object.keys(metadataFacets)) {
       const values = Object.keys(metadataFacets[mfacet]);
