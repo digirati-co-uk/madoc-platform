677f11c1ee3aae344fc50affa5ffc339ce0d3e2b
Heather0K <hkerri200@gmail.com>
2025-01-07 13:10:27 +0000

    reinstate GA block

--- services/madoc-ts/src/extensions/page-blocks/default-definitions.ts
+++ services/madoc-ts/src/extensions/page-blocks/default-definitions.ts
@@ -9,6 +9,7 @@ import simpleHtmlBlock from './simple-html-block/simple-html-block';
 import currentManifest from './current-manifest-snippet-block';
 import simpleMarkdownBlock from './simple-markdown-block/simple-markdown-block';
 import { EmbedItem } from '../../frontend/site/blocks/EmbedItem';
+import { GABlock } from '../../frontend/shared/atoms/GABlock';
 
 export function getDefaultPageBlockDefinitions(): PageBlockDefinition<any, any, any, any>[] {
   return [
@@ -21,6 +22,7 @@ export function getDefaultPageBlockDefinitions(): PageBlockDefinition<any, any, 
     (SingleCollection as any)[Symbol.for('slot-model')] as any,
     (FeaturedItem as any)[Symbol.for('slot-model')] as any,
     (EmbedItem as any)[Symbol.for('slot-model')] as any,
+    (GABlock as any)[Symbol.for('slot-model')] as any,
     simpleMarkdownBlock,
   ];
 }
