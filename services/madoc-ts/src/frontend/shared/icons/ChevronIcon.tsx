a5d3a0868e6adb6355c26f1b2cfa8c9464f90e3f
Heather0K <hkerri200@gmail.com>
2025-03-26 16:57:20 +0000

    new pagination component

--- services/madoc-ts/src/frontend/shared/icons/ChevronIcon.tsx
+++ services/madoc-ts/src/frontend/shared/icons/ChevronIcon.tsx
@@ -2,14 +2,14 @@ import { SVGProps } from 'react';
 import * as React from 'react';
 
 export const ChevronRight = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 96 960 960" width="1em" {...props}>
-    <path d="m304 974-56-57 343-343-343-343 56-57 400 400-400 400Z" />
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 -960 960 900" {...props} fontSize={25}>
+    <path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z" />
   </svg>
 );
 
 export const ChevronLeft = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 96 960 960" width="1em" {...props}>
-    <path d="M400 976 0 576l400-400 56 57-343 343 343 343-56 57Z" />
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 -960 960 900" {...props} fontSize={25}>
+    <path d="M560-240 320-480l240-240 56 56-184 184 184 184-56 56Z" />
   </svg>
 );
 
@@ -17,5 +17,33 @@ export const ChevronDown = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" height="1em" width="1em" {...props}>
     <path d="M24 24H0V0h24v24z" fill="none" />
     <path d="M16.59 8.59 12 13.17 7.41 8.59 6 10l6 6 6-6-1.41-1.41z" />
+  </svg>
+);
+
+export const ChevronFirst = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="#5f6368"
+    viewBox="0 -960 960 900"
+    {...props}
+    fontSize={25}
+  >
+    <path d="M240-240v-480h80v480h-80Zm440 0L440-480l240-240 56 56-184 184 184 184-56 56Z" />
+  </svg>
+);
+
+export const ChevronLast = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="#5f6368"
+    viewBox="0 -960 960 900"
+    {...props}
+    fontSize={25}
+  >
+    <path d="m280-240-56-56 184-184-184-184 56-56 240 240-240 240Zm360 0v-480h80v480h-80Z" />
   </svg>
 );
